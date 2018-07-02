985bc41db70a2d8096bab3ffe35a8823e1aef5e4
felipefreitag <ffvargas@gmail.com>
2018-07-02 20:22:40 -0300

    Add Save button

---
+++ src/favorites/Favorites.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import BookmarkIcon from '@material-ui/icons/Bookmark'
+import Button from '@material-ui/core/Button'
+
+export default props => (
+  <Button variant="raised" color="secondary">
+    <BookmarkIcon />
+    Salvar
+  </Button>
+)
