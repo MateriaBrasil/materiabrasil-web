626e71967679da82475325dbea0417d0b31025f7
felipefreitag <ffvargas@gmail.com>
2018-07-08 16:40:42 -0300

    Add link to user's profile at comments

--- src/comments/renderList.js
+++ src/comments/renderList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'react-router-dom/Link'
 import map from 'lodash/map'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -9,12 +10,14 @@ import PersonIcon from '@material-ui/icons/Person'
 export default props => list => (
   <List>
     {map(list, ({ id, user: { name }, text }) => (
-      <ListItem key={id} button>
-        <Avatar>
-          <PersonIcon />
-        </Avatar>
-        <ListItemText primary={name} secondary={text} />
-      </ListItem>
+      <Link to={`/user/${id}`} style={{ textDecoration: 'none' }}>
+        <ListItem key={id} button>
+          <Avatar>
+            <PersonIcon />
+          </Avatar>
+          <ListItemText primary={name} secondary={text} />
+        </ListItem>
+      </Link>
     ))}
   </List>
 )
