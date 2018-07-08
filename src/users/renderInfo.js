82f4bebc481a862dce716df5bee03c81699f305b
felipefreitag <ffvargas@gmail.com>
2018-07-08 18:57:22 -0300

    Do not show favorites if user has no albums

--- src/users/renderInfo.js
+++ src/users/renderInfo.js
@@ -37,7 +37,7 @@ export default () => props => {
         </Card>
       </Grid>
       <Grid item xs={12}>
-        <Album {...album} />
+        {album && <Album {...album} />}
       </Grid>
     </Grid>
   )
