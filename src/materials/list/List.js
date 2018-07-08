f4203bef889ca5c836ac88bc74021c11ee3afd5f
felipefreitag <ffvargas@gmail.com>
2018-07-08 18:45:12 -0300

    Show save favorite only if logged in

--- src/materials/list/List.js
+++ src/materials/list/List.js
@@ -4,12 +4,15 @@ import Grid from '@material-ui/core/Grid'
 import Card from './Card'
 import NewFavorite from '../../favorites/New'
 
-export default ({ list }) => (
+export default ({ list, currentUser }) => (
   <div>
     <Grid container spacing={24}>
       {list.map((material, index) => (
         <Grid item xs={12} sm={6} md={4} key={index}>
-          <Card {...material} cardAction={<NewFavorite id={material.id} />} />
+          <Card
+            {...material}
+            cardAction={currentUser ? <NewFavorite id={material.id} /> : null}
+          />
         </Grid>
       ))}
     </Grid>
