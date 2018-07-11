cfd1518fcaacd1ec7015bc9dfe68cd49d65e15ff
felipefreitag <ffvargas@gmail.com>
2018-07-11 10:58:51 -0300

    Add star ratings

--- src/reviews/new/Form.js
+++ src/reviews/new/Form.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { reduxForm } from 'redux-form'
-import { required, numericality } from 'redux-form-validators'
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { required } from 'redux-form-validators'
 import Link from 'react-router-dom/Link'
 import Button from '@material-ui/core/Button'
 import DialogActions from '@material-ui/core/DialogActions'
@@ -9,37 +9,39 @@ import DialogContent from '@material-ui/core/DialogContent'
 import Error from '../../Error'
 import TextField from '../../form/TextField'
 
-export default reduxForm({ form: 'reviews' })(props => {
-  const { handleSubmit, onSubmit, error: reduxFormError, createError } = props
-  const { submitting, id } = props
-  const error = reduxFormError || createError
+import Rating from './Rating'
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <DialogContent>
-        <TextField
-          name="rating"
-          label="Nota"
-          type="number"
-          validate={[required(), numericality({ int: true, '>=': 1, '<=': 5 })]}
-        />
-        <TextField
-          name="text"
-          label="Escreva sua avaliação"
-          type="text"
-          multiline
-          validate={[required()]}
-        />
-        {error && <Error>{error}</Error>}
-      </DialogContent>
-      <DialogActions>
-        <Link to={`/${id}`} style={{ textDecoration: 'none' }}>
-          <Button color="primary">Fechar</Button>
-        </Link>
-        <Button color="primary" type="submit" disabled={submitting}>
-          Enviar
-        </Button>
-      </DialogActions>
-    </form>
-  )
-})
+class Form extends Component {
+  render() {
+    const { handleSubmit, onSubmit, error: reduxFormError } = this.props
+    const { createError, submitting, id, change } = this.props
+    const error = reduxFormError || createError
+
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <DialogContent>
+          <Rating onChange={value => change('rating', value)} />
+          <Field component="input" type="hidden" name="rating" />
+          <TextField
+            name="text"
+            label="Escreva sua avaliação"
+            type="text"
+            multiline
+            validate={[required()]}
+          />
+          {error && <Error>{error}</Error>}
+        </DialogContent>
+        <DialogActions>
+          <Link to={`/${id}`} style={{ textDecoration: 'none' }}>
+            <Button color="primary">Fechar</Button>
+          </Link>
+          <Button color="primary" type="submit" disabled={submitting}>
+            Enviar
+          </Button>
+        </DialogActions>
+      </form>
+    )
+  }
+}
+
+export default reduxForm({ form: 'reviews' })(Form)
--- src/reviews/renderNew.js
+++ src/reviews/renderNew.js
@@ -2,7 +2,12 @@ import React from 'react'
 import { New } from 'croods'
 
 import renderNew from './new/render'
+import renderCreated from './new/renderCreated'
 
 export default props => routeProps => (
-  <New name="reviews" render={renderNew({ ...props, ...routeProps })} />
+  <New
+    name="reviews"
+    render={renderNew({ ...props, ...routeProps })}
+    renderCreated={renderCreated({ ...props, ...routeProps })}
+  />
 )
