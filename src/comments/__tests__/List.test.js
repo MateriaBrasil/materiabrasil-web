1189fbe94166578bf46f5e1eb4dc803a028017ab
vractal <v@fractopia.org>
2018-07-26 20:57:42 -0300

    Fix test

--- src/comments/__tests__/List.test.js
+++ src/comments/__tests__/List.test.js
@@ -2,12 +2,27 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 
 import List from '../List'
+import renderList from '../renderList'
+
+
+jest.mock('../renderList', () => jest.fn())
 
 jest.mock('croods', () => ({
-  List: props => <div {...props}>List - {props.children}</div>,
+  List: props => (
+    <div {...props}>
+      List -  {props.children}
+    </div>
+  ),
 }))
 
-it('renders correctly', () => {
-  const tree = renderer.create(<List />).toJSON()
-  expect(tree).toMatchSnapshot()
+describe(List, () => {
+  const tree = renderer.create(<List type="foo" id="bar" />).toJSON()
+
+  it('renders correctly', () => {
+    expect(tree).toMatchSnapshot()
+})
+
+  it('Calls render prop', () => {
+    expect(renderList).toHaveBeenCalledTimes(1)
+  })
 })
