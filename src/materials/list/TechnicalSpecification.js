b1c45779780ed1478b1def5b00bd73ea8b37831b
Daniel Weinmann <danielweinmann@gmail.com>
2018-07-27 10:18:58 -0300

    Layout enhancements

--- src/materials/list/TechnicalSpecification.js
+++ src/materials/list/TechnicalSpecification.js
@@ -1,32 +1,9 @@
 import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
 import CloudDownload from '@material-ui/icons/CloudDownload'
-import Typography from '@material-ui/core/Typography'
 
 export default ({ technicalSpecificationUrl, style }) => (
-  <a
-    href={technicalSpecificationUrl}
-    target="_blank"
-    style={{
-      display: 'inline-block',
-      textDecoration: 'none',
-      marginRight: 24,
-      color: 'white',
-      ...style,
-    }}
-  >
-    <CloudDownload
-      color="inherit"
-      style={{ verticalAlign: 'middle', marginRight: 8 }}
-    />
-    <Typography
-      variant="body1"
-      color="inherit"
-      style={{
-        display: 'inline-block',
-        verticalAlign: 'middle',
-      }}
-    >
-      Ficha técnica
-    </Typography>
-  </a>
+  <IconButton href={technicalSpecificationUrl} target="_blank" style={style}>
+    <CloudDownload color="inherit" />
+  </IconButton>
 )
