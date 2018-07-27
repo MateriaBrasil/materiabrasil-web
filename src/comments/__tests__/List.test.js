4e8c80dce41c62a96f217363ddf515710deec1a9
vractal <v@fractopia.org>
2018-07-27 15:25:40 -0300

    Improve test (2)

--- src/comments/__tests__/List.test.js
+++ src/comments/__tests__/List.test.js
@@ -4,9 +4,8 @@ import renderer from 'react-test-renderer'
 import List from '../List'
 import renderList from '../renderList'
 
-jest.mock('../renderList', () =>
-  jest.fn().mockReturnValue('Render prop - (../renderList)'),
-)
+jest.mock('../renderList', () => jest.fn(list => `renderList - ${list}`))
+
 jest.mock('croods', () => ({
   List: props => <div {...props}>List - {props.children}</div>,
 }))
