0d6d53e4f3b906ee585111b1b996e0bc5ffad422
vractal <v@fractopia.org>
2018-07-31 17:02:32 -0300

    Delete comments

--- src/favorites/__tests__/renderList.test.js
+++ src/favorites/__tests__/renderList.test.js
@@ -11,14 +11,11 @@ jest.mock('../Destroy', () => props => (
   <div {...props}>Destroy - {props.children}</div>
 ))
 
-// Missing TitleBar mock?
-
 it('renders correctly', () => {
   const list = [
     { id: 11111, favoritable: 'favoritable-1', destroying: 'destroying-1' },
     { id: 22222, favoritable: 'favoritable-2', destroying: 'destroying-2' },
   ]
-  // Missing assertion about showDestroy when true and when false
   const props = { showDestroy: true }
   const tree = renderer.create(renderList(props)(list)).toJSON()
   expect(tree).toMatchSnapshot()
