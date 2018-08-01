7026a1595ddbf8424f122cfd855f02fd0979a6f0
felipefreitag <ffvargas@gmail.com>
2018-08-01 12:35:50 -0300

    Remove console.log

--- src/suppliers/new/renderCreated.js
+++ src/suppliers/new/renderCreated.js
@@ -1,7 +1,4 @@
 import React from 'react'
 import Redirect from 'react-router-dom/Redirect'
 
-export default props => supplier => {
-  console.log('created')
-  return <Redirect to={`/`} />
-}
+export default props => supplier => <Redirect to={`/`} />
