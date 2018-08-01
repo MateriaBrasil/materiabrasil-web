627cb770127ba49436970affe6133a44659373ee
felipefreitag <ffvargas@gmail.com>
2018-08-01 12:30:23 -0300

    Add switch to router

--- src/suppliers/Router.js
+++ src/suppliers/Router.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Route from 'react-router-dom/Route'
+import Switch from 'react-router-dom/Switch'
 
 import renderNew from './renderNew'
 
 export default props => (
-  <div>
+  <Switch>
     <Route path="/suppliers/new" render={renderNew(props)} />
-  </div>
+  </Switch>
 )
