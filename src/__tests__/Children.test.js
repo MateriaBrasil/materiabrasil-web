67b4c95b5ba03f94336a5ea1fca058eeb23cc1b6
vractal <v@fractopia.org>
2018-08-04 15:22:10 -0300

    Remove props const

--- src/__tests__/Children.test.js
+++ src/__tests__/Children.test.js
@@ -4,7 +4,6 @@ import renderer from 'react-test-renderer'
 import Children from '../Children'
 
 it('renders correctly', () => {
-  const props = { foo: 'bar', children: 'foo-children' }
-  const tree = renderer.create(<Children>{props.children}</Children>)
+  const tree = renderer.create(<Children>foo-children</Children>)
   expect(tree).toMatchSnapshot()
 })
