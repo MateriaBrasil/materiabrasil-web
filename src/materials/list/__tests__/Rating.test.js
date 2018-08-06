b1eda640e99a105f058804b2870fd1393a0ac14e
vractal <v@fractopia.org>
2018-08-06 15:29:29 -0300

    Create 'Rating' test

---
+++ src/materials/list/__tests__/Rating.test.js
@@ -0,0 +1,8 @@
+import renderer from 'react-test-renderer'
+
+import Rating from '../Rating'
+
+it('renders correctly', () => {
+  const tree = renderer.create(Rating({ averageRating: 4 })).toJSON()
+  expect(tree).toMatchSnapshot()
+})
