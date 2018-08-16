ad6f4fc1455638be1356b3cf5f8e2b718ef90841
felipefreitag <ffvargas@gmail.com>
2018-08-16 11:28:26 -0300

    Get supplier info at material page

--- src/materials/info/Supplier.js
+++ src/materials/info/Supplier.js
@@ -1,38 +1,18 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import SignUpLink from '../../auth/SignUpLink'
-import CaptionWithText from './CaptionWithText'
+
+import { Info } from 'croods'
+
+import renderSupplier from './supplier/render'
 
 export default props => {
-  const { supplierName, manufacturingLocation, averagePrice } = props
-  const { supplierContact, currentUser, location } = props
-  const { pathname } = location
+  const { supplierId } = props
 
   return (
-    <Grid item xs={12} lg={3} style={{ marginBottom: 16 }}>
-      <Card>
-        <CardContent>
-          <CaptionWithText caption="Fornecedor" text={supplierName} />
-          <CaptionWithText
-            caption="Local de produção"
-            text={manufacturingLocation}
-          />
-          <CaptionWithText caption="Preço médio" text={averagePrice} />
-          {currentUser ? (
-            <CaptionWithText
-              caption="Contato do fornecedor"
-              text={supplierContact}
-            />
-          ) : (
-            <SignUpLink
-              pathname={pathname}
-              text="para ver o contato do fornecedor."
-            />
-          )}
-        </CardContent>
-      </Card>
-    </Grid>
+    <Info
+      id={supplierId}
+      name="materialSupplier"
+      path={`/suppliers/${supplierId}`}
+      render={renderSupplier(props)}
+    />
   )
 }
---
+++ src/materials/info/supplier/Info.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+
+import SignUpLink from '../../../auth/SignUpLink'
+import CaptionWithText from '../CaptionWithText'
+
+export default props => {
+  const { manufacturingLocation, averagePrice, supplier } = props
+  const { currentUser, location } = props
+  const { name, email } = supplier
+  const { pathname } = location
+
+  return (
+    <Grid item xs={12} lg={3} style={{ marginBottom: 16 }}>
+      <Card>
+        <CardContent>
+          <CaptionWithText caption="Fornecedor" text={name} />
+          <CaptionWithText
+            caption="Local de produção"
+            text={manufacturingLocation}
+          />
+          <CaptionWithText caption="Preço médio" text={averagePrice} />
+          {currentUser ? (
+            <CaptionWithText caption="Contato do fornecedor" text={email} />
+          ) : (
+            <SignUpLink
+              pathname={pathname}
+              text="para ver o contato do fornecedor."
+            />
+          )}
+        </CardContent>
+      </Card>
+    </Grid>
+  )
+}
