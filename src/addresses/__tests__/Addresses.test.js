4ab311276275b43880d831bb0c5ae44f320c1e84
Samirah Sessim <samirahcsessim@gmail.com>
2018-08-22 17:40:02 -0300

    fix-test-address

--- src/addresses/__tests__/Addresses.test.js
+++ src/addresses/__tests__/Addresses.test.js
@@ -4,30 +4,29 @@ import renderer from 'react-test-renderer'
 import Addresses from '../Addresses'
 
 jest.mock('react-router-dom/Link', () => props => (
-    <div {...props}>Link - {props.children}</div>
+  <div {...props}>Link - {props.children}</div>
 ))
-jest.mock('../New', () => props => (
-    <div {...props}>New - {props.children}</div>
-))
+jest.mock('../New', () => props => <div {...props}>New - {props.children}</div>)
 jest.mock('../List', () => props => (
-    <div {...props}>List - {props.children}</div>
+  <div {...props}>List - {props.children}</div>
 ))
-
 
 it('renders correctly', () => {
-    const pathname = 'foo'
-    const location =  { pathname }
-    const props = { location }
-    const tree = renderer.create(<Addresses {...props} />).toJSON()
-    expect(tree).toMatchSnapshot()
-}) 
+  const pathname = 'foo'
+  const location = { pathname }
+  const props = { location }
+  const tree = renderer.create(<Addresses {...props} />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
 
 describe('with current user', () => {
-    it('renders correctly', () => {
-        const pathname = 'foo'
-        const location =  { pathname }
-        const props = { location }
-        const tree = renderer.create(<Addresses currentUser={{}} supplier={{}} {...props}/>)
-        expect(tree).toMatchSnapshot()
-    }) 
-}) 
+  it('renders correctly', () => {
+    const pathname = 'foo'
+    const location = { pathname }
+    const props = { location }
+    const tree = renderer.create(
+      <Addresses currentUser={{}} supplier={{}} {...props} />,
+    )
+    expect(tree).toMatchSnapshot()
+  })
+})
