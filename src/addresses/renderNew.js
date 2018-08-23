3c0fad9ff8b3575f35e99200d56845fa79c5c4c1
vractal <v@fractopia.org>
2018-08-23 12:37:29 -0300

    Implement card in renderNew

--- src/addresses/renderNew.js
+++ src/addresses/renderNew.js
@@ -1,5 +1,7 @@
-import React from 'react'
-
+import React, { Fragment } from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 import Form from './Form'
 import createWithParams from './createWithParams'
 
@@ -7,11 +9,25 @@ export default props => ({ create, creating, error }) => {
   const { id } = props
 
   return (
-    <Form
-      {...props}
-      onSubmit={createWithParams(create, id)}
-      creating={creating}
-      createError={error}
-    />
+    <Card>
+      <CardContent>
+        <Typography
+          variant="headline"
+          color="textSecondary"
+          style={{ marginBottom: 16 }}
+        >
+          Adicionar endereço
+        </Typography>
+        <Fragment>
+          <Form
+            {...props}
+            onSubmit={createWithParams(create, id)}
+            creating={creating}
+            createError={error}
+          />
+        </Fragment>
+      </CardContent>
+    </Card>
+
   )
 }
