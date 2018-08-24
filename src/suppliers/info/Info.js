abac858b9be12e9aca9d4e7598a0114f48e72362
vractal <v@fractopia.org>
2018-08-24 16:22:41 -0300

    Refactor Info

--- src/suppliers/info/Info.js
+++ src/suppliers/info/Info.js
@@ -1,13 +1,9 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import queryString from 'query-string'
 
-import SignUpLink from '../../auth/SignUpLink'
-import PropertyWithValue from '../../PropertyWithValue'
-import translateReach from './translateReach'
+import InfoCard from './InfoCard'
 import Avatar from '../../imageUpload/Avatar'
 import Addresses from '../../addresses/Addresses'
 import Materials from './Materials'
@@ -26,11 +22,9 @@ class Info extends Component {
     }
   }
   render() {
-    const { current, currentUser, location } = this.props
-    const { pathname } = location
-    const { id, name, description, email, phone, reach, imageUrl } = current
+    const { current, currentUser } = this.props
     const { userId, materials } = current
-
+    const { id, name, imageUrl } = current
     const editPath =
       currentUser && userId === currentUser.id && `/suppliers/${id}/avatar`
 
@@ -46,41 +40,7 @@ class Info extends Component {
           </Typography>
         </Grid>
         <Grid item xs={12} lg={8}>
-          <Card>
-            <CardContent>
-              <PropertyWithValue
-                title="Descrição"
-                value={description}
-                style={{ marginTop: 16 }}
-              />
-              <PropertyWithValue
-                title="Alcance"
-                value={translateReach(reach)}
-                style={{ marginTop: 16 }}
-              />
-              {currentUser ? (
-                <Fragment>
-                  <PropertyWithValue
-                    title="Email"
-                    value={email}
-                    style={{ marginTop: 16 }}
-                  />
-                  <PropertyWithValue
-                    title="Telefone"
-                    value={phone}
-                    style={{ marginTop: 16 }}
-                  />
-                </Fragment>
-              ) : (
-                <div style={{ marginTop: 16 }}>
-                  <SignUpLink
-                    pathname={pathname}
-                    text="para ver o contato do fornecedor."
-                  />
-                </div>
-              )}
-            </CardContent>
-          </Card>
+          <InfoCard {...this.props} />
         </Grid>
         <Grid item xs={12}>
           {materials && <Materials list={materials} />}
