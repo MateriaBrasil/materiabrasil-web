0f9a4051333d91208dabafde548cbf4a213855b2
vractal <v@fractopia.org>
2018-08-24 15:02:52 -0300

    Add renderEdit to router

---
+++ src/suppliers/renderEdit.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Edit } from 'croods'
+import Redirect from 'react-router-dom/Redirect'
+//
+import render from './edit/render'
+
+export default props => routeProps => {
+  const { match } = routeProps
+  const { params } = match
+  const { id } = params
+  return (
+    <Edit
+      id={id}
+      name="suppliers"
+      render={render({ ...props, ...routeProps })}
+      renderUpdated={() => <Redirect to={'/suppliers/${id}'} />}
+    />)
+
+}
