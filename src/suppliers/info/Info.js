698c28156ad51f4847fc164f2bd458ad85036701
vractal <v@fractopia.org>
2018-08-27 16:56:09 -0300

    Refactor info

--- src/suppliers/info/Info.js
+++ src/suppliers/info/Info.js
@@ -17,21 +17,16 @@ class Info extends Component {
   }
   render() {
     const { current, currentUser } = this.props
-    const { userId, materials } = current
-    const { id, name, imageUrl } = current
+    const { userId, materials, id, name, imageUrl } = current
     const editPath =
       currentUser && userId === currentUser.id && `/suppliers/${id}/avatar`
-
     const editProfile =
-      currentUser && currentUser.suppliers
-        ? currentUser.suppliers[0].id.toString() === id.toString()
-        : null
+      currentUser && currentUser.id.toString() === userId.toString()
 
     return (
       <Grid container spacing={32}>
         <Grid item xs={12} lg={4}>
           <Avatar name={name} editPath={editPath} imageUrl={imageUrl} />
-
           <Typography
             variant="display1"
             style={{ marginTop: 16, marginBottom: 16 }}
