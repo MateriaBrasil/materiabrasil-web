b607be0902e126b751da30923a2cf69e277a313e
vractal <v@fractopia.org>
2018-08-29 09:32:16 -0300

    Make logo responsive

--- src/navBar/Logo.js
+++ src/navBar/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'react-router-dom/Link'
 import Typography from '@material-ui/core/Typography'
+import './Logo.css'
 
 export default () => (
-  <div style={{ flex: 1 }}>
+  <div className="logo" style={{ flex: 1 }}>
     <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
       <img
+        className="image"
         src="/images/logo-small.jpg"
         alt="logo"
         style={{
@@ -17,10 +19,11 @@ export default () => (
       <Typography
         variant="display2"
         color="inherit"
+        className="text"
         style={{
           display: 'inline-block',
           verticalAlign: 'middle',
-          fontSize: 36,
+
         }}
       >
         Materia<span style={{ fontWeight: 300 }}>Mundi</span>
