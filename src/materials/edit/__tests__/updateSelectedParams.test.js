6b244481833565ca27dce2bf15b1bda94896f06b
felipefreitag <ffvargas@gmail.com>
2018-08-29 10:51:15 -0300

    Increase coverage

---
+++ src/materials/edit/__tests__/updateSelectedParams.test.js
@@ -0,0 +1,22 @@
+import UpdateSelectedParams from '../updateSelectedParams'
+
+const update = jest.fn()
+const params = {
+  id: 123,
+  name: 'Foo material',
+  code: 234,
+  averagePrice: 'R$ 10',
+  imageUrl: 'foo image',
+  coverImageUrl: 'http://bar.image',
+  listImageUrl: 'http://list.image',
+  highlightImageUrl: 'http://highlight.image',
+  description: 'Foo description',
+  properties: 'Foo properties',
+  usage: 'Bar usage',
+  technicalSpecificationUrl: 'http://foo',
+}
+
+it('calls correctly', () => {
+  UpdateSelectedParams(update)(params)
+  expect(update).toHaveBeenCalledWith(params)
+})
