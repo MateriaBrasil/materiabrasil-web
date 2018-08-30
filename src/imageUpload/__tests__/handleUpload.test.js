5827d12d00d5e86fa249e54cd1215d4533df985c
vractal <v@fractopia.org>
2018-08-30 11:25:10 -0300

    Create handleUpload test

---
+++ src/imageUpload/__tests__/handleUpload.test.js
@@ -0,0 +1,23 @@
+import handleUpload from '../handleUpload'
+
+import s3HandleUpload from '../../s3Upload/handleUpload'
+
+jest.mock('../../s3Upload/handleUpload', () => jest.fn())
+jest.mock('../parseCanvas', () => jest.fn(props => props))
+
+it('calls function correctly', async () => {
+  const update = jest.fn()
+  await handleUpload({
+    editorImage: { md5: '1a79a4d', blob: 'foo-blob'},
+    update,
+    id: 1234,
+    attributeName: 'foo-attr',
+  })
+  expect(s3HandleUpload).toHaveBeenCalledWith({
+    Key: `uploads//1a79a4d.jpg`,
+    Body: 'foo-blob',
+    update,
+    id: 1234,
+    attributeName: 'foo-attr',
+  })
+})
