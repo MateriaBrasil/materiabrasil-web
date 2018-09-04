3a6df92baa9d6693c18ee8cafded446aaca99bbf
vractal <v@fractopia.org>
2018-09-04 17:59:42 -0300

    Refactor Icons

--- src/materials/info/CertificationsIcon.js
+++ src/materials/info/CertificationsIcon.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import IconButton from '@material-ui/core/IconButton'
-import CloudDone from '@material-ui/icons/CloudDone'
+import FileDownloadIcon from '../FileDownloadIcon'
 
 export default ({ certificationsUrl, style }) => (
-  <IconButton href={certificationsUrl} target="_blank" style={style}>
-    <CloudDone color="inherit" />
-  </IconButton>
+  <FileDownloadIcon url={certificationsUrl} style={style} />
 )
--- src/materials/list/TechnicalSpecification.js
+++ src/materials/list/TechnicalSpecification.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import IconButton from '@material-ui/core/IconButton'
-import CloudDownload from '@material-ui/icons/CloudDownload'
+
+import FileDownloadIcon from '../FileDownloadIcon'
 
 export default ({ technicalSpecificationUrl, style }) => (
-  <IconButton href={technicalSpecificationUrl} target="_blank" style={style}>
-    <CloudDownload color="inherit" />
-  </IconButton>
+  <FileDownloadIcon url={technicalSpecificationUrl} style={style} />
 )
