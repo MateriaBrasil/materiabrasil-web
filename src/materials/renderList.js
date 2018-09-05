a482bb1324addefe99c93a035ac225a1f72fe1ba
Gustavo Guichard <gustavoguichard@gmail.com>
2018-09-05 17:41:00 -0300

    Fix lint

--- src/materials/renderList.js
+++ src/materials/renderList.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { List } from 'croods'
 import Grid from '@material-ui/core/Grid'
 
+import renderCategoriesList from 'categories/list/render'
 import Search from '../search/Search'
 import renderList from './list/render'
-import renderCategoriesList from 'categories/list/render'
 
 export default props => ({ history }) => (
   <Grid container spacing={16}>
     <Grid item xs={12} md={3}>
       <Search history={history} />
-      <List name="categories" render={renderCategoriesList({ ...props, history })} />
+      <List
+        name="categories"
+        render={renderCategoriesList({ ...props, history })}
+      />
     </Grid>
     <Grid item xs={12} md={9}>
       <List name="materials" render={renderList(props)} />
