bc4953080acf2952e418c6f351bab5eadc973db1
Daniel Weinmann <danielweinmann@gmail.com>
2018-09-05 15:56:48 -0300

    Put the listing inside the dialog

--- src/categories/Categories.js
+++ src/categories/Categories.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import FormGroup from '@material-ui/core/FormGroup'
 
-import Dialog from 'materials/Dialog'
-
 import renderCategory from './renderCategory'
 
 export default ({ categories, ...props }) => (
-  <Dialog {...props} title="Editar informações técnicas">
-    <FormGroup row={false}>{categories.map(renderCategory(props))}</FormGroup>
-  </Dialog>
+  <FormGroup row={false}>{categories.map(renderCategory(props))}</FormGroup>
 )
--- src/categories/render.js
+++ src/categories/render.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { List } from 'croods'
 
+import Dialog from 'materials/Dialog'
 import renderList from './renderList'
 
 export default props => routeProps => (
-  <List name="categories" render={renderList({ ...props, ...routeProps })} />
+  <Dialog {...props} {...routeProps} title="Editar informações técnicas">
+    <List name="categories" render={renderList({ ...props, ...routeProps })} />
+  </Dialog>
 )
