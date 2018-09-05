07bc545aea8043ebb973dc6d7d5a36330d9e27ce
Gustavo Guichard <gustavoguichard@gmail.com>
2018-09-05 16:48:12 -0300

    Reuse renderCategory on sidebar

--- src/categories/list/List.js
+++ src/categories/list/List.js
@@ -1,19 +1,3 @@
-import React, { Component } from 'react'
+import renderCategory from 'categories/renderCategory'
 
-export default class extends Component {
-  // componentDidUpdate(prevProps) {
-  //   const { location, listProps } = this.props
-  //   const { location: prevLocation } = prevProps
-
-  //   if (listProps && location.search !== prevLocation.search) {
-  //     const { actions } = listProps
-  //     actions.fetchList()
-  //   }
-  // }
-
-  render() {
-    const { list } = this.props
-    console.log(list)
-    return <div className={list} />
-  }
-}
+export default props => props.list.map(renderCategory(props))
--- src/materials/renderList.js
+++ src/materials/renderList.js
@@ -10,7 +10,7 @@ export default props => ({ history }) => (
   <Grid container spacing={0}>
     <Grid item xs={12} md={3}>
       <Search history={history} />
-      <List name="categories" render={renderCategoriesList(props)} />;
+      <List name="categories" render={renderCategoriesList(props)} />
     </Grid>
     <Grid item xs={12} md={9}>
       <List name="materials" render={renderList(props)} />;
