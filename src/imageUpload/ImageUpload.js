f74b0b00196d50633c3c1499e2302af6283893db
vractal <v@fractopia.org>
2018-09-08 14:46:45 -0300

    Refactor ImageUpload

--- src/imageUpload/ImageUpload.js
+++ src/imageUpload/ImageUpload.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
-import Dropzone from 'react-dropzone'
-import AvatarEditor from 'react-avatar-editor'
-import Typography from '@material-ui/core/Typography'
 
-import theme from '../theme'
 import Dialog from './Dialog'
 import ZoomControls from './ZoomControls'
 import handleUpload from './handleUpload'
+import ImageDropZone from './ImageDropZone'
 
 export default class extends Component {
   constructor(props) {
@@ -67,7 +64,17 @@ export default class extends Component {
   render() {
     const { file, scale, position, uploading } = this.state
     const { updating, title, width = 400, height = 400, ...props } = this.props
-
+    const dropzoneProps = {
+      file,
+      scale,
+      position,
+      title,
+      width,
+      height,
+      onDrop: this.onDrop,
+      setEditor: this.setEditor,
+      onPositionChange: this.onPositionChange,
+    }
     return (
       <Dialog
         {...props}
@@ -76,49 +83,7 @@ export default class extends Component {
         cancelDisabled={updating || uploading}
         uploading={uploading}
       >
-        <Dropzone
-          accept="image/*"
-          multiple={false}
-          disableClick={!!file}
-          onDrop={this.onDrop}
-          style={{
-            textAlign: 'center',
-            border: file ? 'none' : '1px dashed',
-            width,
-            height,
-            maxHeight: '100%',
-            maxWidth: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            margin: '0 auto',
-            cursor: file ? 'move' : 'pointer',
-          }}
-          acceptStyle={{ border: `2px solid ${theme.palette.primary.main}` }}
-          rejectStyle={{ border: `2px solid ${theme.palette.error.main}` }}
-        >
-          {file ? (
-            <AvatarEditor
-              ref={this.setEditor}
-              width={width - 50}
-              height={height - 50}
-              image={file.preview}
-              scale={scale}
-              position={position}
-              onPositionChange={this.onPositionChange}
-            />
-          ) : (
-            <Typography
-              variant="headline"
-              style={{
-                width: 300,
-                position: 'relative',
-                top: height / 2 - 60,
-              }}
-            >
-              {title || 'Arraste uma imagem ou clique para escolher o arquivo.'}
-            </Typography>
-          )}
-        </Dropzone>
+        <ImageDropZone {...dropzoneProps} />
         {file && (
           <ZoomControls onZoomOut={this.onZoomOut} onZoomIn={this.onZoomIn} />
         )}
