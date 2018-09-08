b524a93550ca52474acec797d7cce584f1e9a5c8
vractal <v@fractopia.org>
2018-09-08 15:48:13 -0300

    Refactor Dialog

--- src/imageUpload/Dialog.js
+++ src/imageUpload/Dialog.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import withMobileDialog from '@material-ui/core/withMobileDialog'
-import Button from '@material-ui/core/Button'
 
 import Loading from '../Loading'
 import closeDialog from './closeDialog'
+import ImageDialogActions from './ImageDialogActions'
 
 export default withMobileDialog()(
   class extends Component {
     render() {
-      const { fullScreen, disabled, onUpload, children, uploading } = this.props
-      const { cancelDisabled } = this.props
+      const { fullScreen, children, uploading } = this.props
       const handleCloseDialog = closeDialog(this.props)
-
       return (
         <Dialog
           fullScreen={fullScreen}
@@ -29,18 +26,10 @@ export default withMobileDialog()(
           <DialogTitle id="responsive-dialog-title">Alterar imagem</DialogTitle>
           <DialogContent>{children}</DialogContent>
           {uploading && <Loading />}
-          <DialogActions>
-            <Button
-              disabled={cancelDisabled}
-              onClick={handleCloseDialog}
-              color="primary"
-            >
-              Cancelar
-            </Button>
-            <Button disabled={disabled} color="primary" onClick={onUpload}>
-              Salvar
-            </Button>
-          </DialogActions>
+          <ImageDialogActions
+            {...this.props}
+            handleCloseDialog={handleCloseDialog}
+          />
         </Dialog>
       )
     }
