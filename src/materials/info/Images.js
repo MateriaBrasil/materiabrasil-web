f048650e35552675d44a33741ba47b613116bd60
vractal <v@fractopia.org>
2018-09-09 19:04:42 -0300

    Refactor Images

--- src/materials/info/Images.js
+++ src/materials/info/Images.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 
-import Avatar from '../../imageUpload/Avatar'
+import Image from './Image'
 
 export default ({ id, name, listImageUrl, highlightImageUrl }) => {
   return (
@@ -15,32 +15,24 @@ export default ({ id, name, listImageUrl, highlightImageUrl }) => {
             Imagens
           </Typography>
           <Grid container spacing={24}>
-            <Grid item xs={12} lg={4}>
-              <Typography variant="headline" style={{ marginBottom: 16 }}>
-                Imagem para a lista
-              </Typography>
-              <Avatar
-                name={name}
-                editPath={`/${id}/listImage`}
-                imageUrl={listImageUrl}
-                width={720}
-                height={540}
-                preserveRatio
-              />
-            </Grid>
-            <Grid item xs={12} lg={8}>
-              <Typography variant="headline" style={{ marginBottom: 16 }}>
-                Imagem para os destaques
-              </Typography>
-              <Avatar
-                name={name}
-                editPath={`/${id}/highlightImage`}
-                imageUrl={highlightImageUrl}
-                width={1920}
-                height={720}
-                preserveRatio
-              />
-            </Grid>
+            <Image
+              width={720}
+              height={540}
+              editPath={`/${id}/listImage`}
+              name={name}
+              lg={4}
+              imageUrl={listImageUrl}
+              text="Imagem para a lista"
+            />
+            <Image
+              editPath={`/${id}/highlightImage`}
+              name={name}
+              lg={8}
+              imageUrl={highlightImageUrl}
+              text="Imagem para os destaques"
+              width={1920}
+              height={720}
+            />
           </Grid>
         </CardContent>
       </Card>
