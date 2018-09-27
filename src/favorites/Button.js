5951262e2962e9229d0cec0d4ed0040838ad8ad5
Rodrigo <roavellarm@gmail.com>
2018-09-27 15:46:44 -0300

    Insert text to tooltips icons

--- src/favorites/Button.js
+++ src/favorites/Button.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import BookmarkIcon from '@material-ui/icons/Bookmark'
 import IconButton from '@material-ui/core/IconButton'
-
+import Tooltip from '@material-ui/core/Tooltip'
 import Loading from './Loading'
 
 export default ({ onClick, creating, style }) =>
   creating ? (
     <Loading />
   ) : (
-    <IconButton onClick={onClick} style={style}>
-      <BookmarkIcon />
-    </IconButton>
+    <Tooltip title="Adicionar aos favoritos">
+      <IconButton onClick={onClick} style={style}>
+        <BookmarkIcon />
+      </IconButton>
+    </Tooltip>
   )
--- src/materials/FileDownloadIcon.js
+++ src/materials/FileDownloadIcon.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import CloudDownload from '@material-ui/icons/CloudDownload'
+import Tooltip from '@material-ui/core/Tooltip'
 
-export default ({ url, style }) => (
-  <IconButton href={url} target="_blank" style={style}>
-    <CloudDownload color="inherit" />
-  </IconButton>
+export default ({ url, style, tooltip_title }) => (
+  <Tooltip title={tooltip_title}>
+    <IconButton href={url} target="_blank" style={style}>
+      <CloudDownload color="inherit" />
+    </IconButton>
+  </Tooltip>
 )
