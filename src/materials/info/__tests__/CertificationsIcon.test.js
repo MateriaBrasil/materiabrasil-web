a3d1ee439ca6b2155a15ce165c602ce71b9d0119
Peterson Passos <peterson.jfp@gmail.com>
2018-09-28 14:53:36 -0300

    adjusted tests to recognize the new changes

--- src/materials/info/__tests__/CertificationsIcon.test.js
+++ src/materials/info/__tests__/CertificationsIcon.test.js
@@ -3,7 +3,9 @@ import renderer from 'react-test-renderer'
 
 import CertificationsIcon from '../CertificationsIcon'
 
-jest.mock('../../FileDownloadIcon', () => props => <div {...props}>FileDownloadIcon</div> )
+jest.mock('../../FileDownloadIcon', () => props => (
+  <div {...props}>FileDownloadIcon</div>
+))
 
 it('renders correctly', () => {
   const props = {
--- src/materials/list/__tests__/TechnicalSpecification.test.js
+++ src/materials/list/__tests__/TechnicalSpecification.test.js
@@ -3,7 +3,9 @@ import renderer from 'react-test-renderer'
 
 import TechnicalSpecification from '../TechnicalSpecification'
 
-jest.mock('../../FileDownloadIcon', () => props => <div {...props}>FileDownloadIcon</div> )
+jest.mock('../../FileDownloadIcon', () => props => (
+  <div {...props}>FileDownloadIcon</div>
+))
 
 it('renders correctly', () => {
   const props = {
