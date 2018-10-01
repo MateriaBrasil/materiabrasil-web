6358be8a72d9e2442dbe7c0ae1c8ee24994b61ae
Peterson Passos <peterson.jfp@gmail.com>
2018-10-01 10:20:06 -0300

    add tests to FileDownloadIcon.js

---
+++ src/materials/__tests__/FileDownloadIcon.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import FileDownloadIcon from '../FileDownloadIcon'
+
+jest.mock('@material-ui/core/Tooltip', () => props => (
+  <div {...props}>Tooltip - {props.children}</div>
+))
+
+it('renders correctly', () => {
+    const props = {
+        url: 'foo-url',
+        style: { foo: 'style' },
+        tooltip: 'foo-text',
+      }
+      const tree = renderer.create(<FileDownloadIcon {...props} />).toJSON()
+      expect(tree).toMatchSnapshot()
+})
