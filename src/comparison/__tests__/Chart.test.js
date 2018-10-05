41abbb3649cdbd14db9156c749e034ccc500f9f5
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-05 17:40:07 -0300

    Fix lint warnings

--- src/comparison/__tests__/Chart.test.js
+++ src/comparison/__tests__/Chart.test.js
@@ -4,11 +4,11 @@ import renderer from 'react-test-renderer'
 import Chart from '../Chart'
 
 const props = {
-    list: [{id: 1, name: 'foo'}],
-    actions:{close: jest.fn()}
+  list: [{ id: 1, name: 'foo' }],
+  actions: { close: jest.fn() },
 }
 
 it('renders correctly', () => {
-    const tree = renderer.create(<Chart {...props}/>).toJSON()
-    expect(tree).toMatchSnapshot()
+  const tree = renderer.create(<Chart {...props} />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
--- src/comparison/__tests__/actions.test.js
+++ src/comparison/__tests__/actions.test.js
@@ -1,33 +1,33 @@
-import {reset,add, remove, open, close} from '../actions'
+import { reset, add, remove, open, close } from '../actions'
 
 describe('reset', () => {
-    it('returns as expected', () => {
-        expect(reset()).toMatchObject({ type: '@comparison/RESET' })
-    } )
+  it('returns as expected', () => {
+    expect(reset()).toMatchObject({ type: '@comparison/RESET' })
+  })
 })
 
 describe('add', () => {
-    it('returns as expected', () => {
-        const item = { foobar: 'foo' }
-        expect(add(item)).toMatchObject({ type: '@comparison/ADD', item })
-    } )
+  it('returns as expected', () => {
+    const item = { foobar: 'foo' }
+    expect(add(item)).toMatchObject({ type: '@comparison/ADD', item })
+  })
 })
 
 describe('remove', () => {
-    it('returns as expected', () => {
-        const item = { foobar: 'foo' }
-        expect(remove(item)).toMatchObject({ type: '@comparison/REMOVE', item })
-    } )
+  it('returns as expected', () => {
+    const item = { foobar: 'foo' }
+    expect(remove(item)).toMatchObject({ type: '@comparison/REMOVE', item })
+  })
 })
 
 describe('open', () => {
-    it('returns as expected', () => {
-        expect(open()).toMatchObject({ type: '@comparison/OPEN' })
-    } )
+  it('returns as expected', () => {
+    expect(open()).toMatchObject({ type: '@comparison/OPEN' })
+  })
 })
 
 describe('close', () => {
-    it('returns as expected', () => {
-        expect(close()).toMatchObject({ type: '@comparison/CLOSE' })
-    } )
+  it('returns as expected', () => {
+    expect(close()).toMatchObject({ type: '@comparison/CLOSE' })
+  })
 })
