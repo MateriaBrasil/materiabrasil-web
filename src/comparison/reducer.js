bcea2e8e9817cbd228dcf423ba043308dd8434ec
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-05 20:42:54 -0300

    Fragmenting method in src/comparison/reducer

--- src/comparison/reducer.js
+++ src/comparison/reducer.js
@@ -10,30 +10,46 @@ export default function reducer(state = initialState, action = {}) {
     case '@comparison/RESET':
       return initialState
     case '@comparison/ADD':
-      return {
-        ...state,
-        list: [
-          ...filter(state.list, ({ id }) => id !== action.item.id),
-          action.item,
-        ],
-        open: state.open || state.list.length > 0,
-      }
+      return add(state, action)
     case '@comparison/REMOVE':
-      return {
-        ...state,
-        list: filter(state.list, ({ id }) => id !== action.item.id),
-      }
+      return remove(state, action)
     case '@comparison/OPEN':
-      return {
-        ...state,
-        open: true,
-      }
+      return open(state, action)
     case '@comparison/CLOSE':
-      return {
-        ...state,
-        open: false,
-      }
+      return close(state, action)
     default:
       return state
+  }
+}
+
+function add(state, action) {
+  return {
+    ...state,
+    list: [
+      ...filter(state.list, ({ id }) => id !== action.item.id),
+      action.item,
+    ],
+    open: state.open || state.list.length > 0,
+  }
+}
+
+function remove(state, action) {
+  return {
+    ...state,
+    list: filter(state.list, ({ id }) => id !== action.item.id),
+  }
+}
+
+function open(state, action) {
+  return {
+    ...state,
+    open: true,
+  }
+}
+
+function close(state, action) {
+  return {
+    ...state,
+    open: false,
   }
 }
