c3d0b9f4b4ed57b8ab383a64d6a9339cad57f838
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-05 17:03:19 -0300

    WIP - Add comparisson/chart.test

---
+++ src/comparison/__tests__/Chart.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Chart from '../Chart'
+
+const props = {
+    list: [{id: 1, name: 'foo'}],
+    actions:{close: jest.fn()}
+}
+
+it('renders correctly', () => {
+    const tree = renderer.create(<Chart {...props}/>).toJSON()
+    expect(tree).toMatchSnapshot()
+})
