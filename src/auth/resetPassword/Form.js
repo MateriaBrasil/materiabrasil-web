3ad78d8b00690733733ccc7164491ba6028c23ae
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-09 18:09:16 -0300

    Add ResetPassword Component

---
+++ src/auth/resetPassword/Form.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { reduxForm } from 'redux-form'
+import CardHeader from '@material-ui/core/CardHeader'
+import Button from '@material-ui/core/Button'
+import { required } from 'redux-form-validators'
+import SingleColumn from '../../SingleColumn'
+import DefaultForm from '../../form/Form'
+
+import TextField from '../../form/TextField'
+
+import Error from 'Error'
+
+const Form = props => {
+  const { onSubmit, title, callToAction, error, submitting } = props
+
+  return (
+    <SingleColumn>
+      <DefaultForm callToAction="String" error={error}>
+        <form onSubmit={onSubmit}>
+          {title && <CardHeader title={title} />}
+          <TextField
+            name="email"
+            label="email"
+            type="email"
+            validate={[required()]}
+          />
+          <Error>{error}</Error>
+        </form>
+      </DefaultForm>
+    </SingleColumn>
+  )
+}
+
+export default reduxForm({ form: 'resetPassword' })(Form)
---
+++ src/auth/resetPassword/render.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import ResetPassword from './ResetPassword'
+
+export default props => routeProps => (
+  <ResetPassword {...props} {...routeProps} />
+)
