4af530dae41f0c8f032e59e664e975fbf3d28f70
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-09 18:08:44 -0300

    Add forgotPassword route

--- src/auth/Router.js
+++ src/auth/Router.js
@@ -4,10 +4,16 @@ import Switch from 'react-router-dom/Switch'
 
 import renderSignIn from './signIn/render'
 import renderSignUp from './signUp/render'
+import renderResetPassword from './resetPassword/render'
 
 export default props => (
   <Switch>
     <Route exact path="/auth/sign-in" render={renderSignIn(props)} />
     <Route exact path="/auth/sign-up" render={renderSignUp(props)} />
+    <Route
+      exact
+      path="/auth/forgot-password"
+      render={renderResetPassword(props)}
+    />
   </Switch>
 )
