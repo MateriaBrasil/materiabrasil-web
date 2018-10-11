e01b072286e072b3df564dc39d4058cac8e0c232
Giovani <sousa_giovani@hotmail.com>
2018-10-11 16:52:36 -0300

    Send redirect_url

--- src/auth/forgotPassword/Form.js
+++ src/auth/forgotPassword/Form.js
@@ -3,17 +3,17 @@ import { reduxForm } from 'redux-form'
 import CardHeader from '@material-ui/core/CardHeader'
 import { required } from 'redux-form-validators'
 import Button from '@material-ui/core/Button'
+
 import Error from 'Error'
-
 import SingleColumn from '../../SingleColumn'
-
 import TextField from '../../form/TextField'
+import createWithParams from './createWithParams'
 
 export default reduxForm({ form: 'forgotPassword' })(props => {
   const { title, handleSubmit, create, creating, error } = props
   return (
     <SingleColumn>
-      <form onSubmit={handleSubmit(create)}>
+      <form onSubmit={handleSubmit(createWithParams(create))}>
         {title && <CardHeader title={title} />}
         <TextField
           name="email"
