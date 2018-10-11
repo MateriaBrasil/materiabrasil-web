6bd94dc197356d5b00b1bcc9d3e5e9f18537f058
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-10 23:29:08 -0300

    Refactor resetPassword Form

--- src/auth/resetPassword/Form.js
+++ src/auth/resetPassword/Form.js
@@ -2,38 +2,42 @@ import React from 'react'
 import { reduxForm } from 'redux-form'
 import CardHeader from '@material-ui/core/CardHeader'
 import { required } from 'redux-form-validators'
+import Button from '@material-ui/core/Button'
 import Error from 'Error'
 
 import SingleColumn from '../../SingleColumn'
-import DefaultForm from '../../form/Form'
 
 import TextField from '../../form/TextField'
 
-const Form = props => {
-  const { onSubmit, title, error } = props
-
+export default reduxForm({ form: 'resetPassword' })(props => {
+  const { title, handleSubmit, create, creating, error } = props
   return (
     <SingleColumn>
-      <DefaultForm callToAction="Recuperar senha" error={error}>
-        <form onSubmit={onSubmit}>
-          {title && <CardHeader title={title} />}
-          <TextField
-            name="password"
-            label="Senha"
-            type="password"
-            validate={[required()]}
-          />
-          <TextField
-            name="passwordConfirmation"
-            label="Confirmar senha"
-            type="password"
-            validate={[required()]}
-          />
-          <Error>{error}</Error>
-        </form>
-      </DefaultForm>
+      <form onSubmit={handleSubmit(create)}>
+        {title && <CardHeader title={title} />}
+        <TextField
+          name="password"
+          label="Senha"u
+          type="password"
+          validate={[required()]}
+        />
+        <TextField
+          name="passwordConfirmation"
+          label="Confirmar senha"
+          type="password"
+          validate={[required()]}
+        />
+        <Error>{error}</Error>
+        <Button
+          variant="raised"
+          color="primary"
+          type="submit"
+          disabled={creating}
+          style={{ width: '100%' }}
+        >
+          Alterar senha
+        </Button>
+      </form>
     </SingleColumn>
   )
-}
-
-export default reduxForm({ form: 'forgotPassword' })(Form)
+})
