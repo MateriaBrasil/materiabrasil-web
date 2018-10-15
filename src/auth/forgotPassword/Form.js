76d18a877b5cbc82b47b874373e10aedffaaaf38
Giovani <sousa_giovani@hotmail.com>
2018-10-15 09:23:52 -0300

    add email validator

--- src/auth/forgotPassword/Form.js
+++ src/auth/forgotPassword/Form.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { reduxForm } from 'redux-form'
 import CardHeader from '@material-ui/core/CardHeader'
-import { required } from 'redux-form-validators'
+import Validators, { required, email } from 'redux-form-validators'
 import Button from '@material-ui/core/Button'
 
 import Error from 'Error'
@@ -11,6 +11,14 @@ import createWithParams from './createWithParams'
 
 export default reduxForm({ form: 'forgotPassword' })(props => {
   const { title, handleSubmit, create, creating, error } = props
+
+  Object.assign(Validators.messages, {
+    email: {
+      id: 'form.errors.email',
+      defaultMessage: 'Não é um email válido',
+    },
+  })
+
   return (
     <SingleColumn>
       <form onSubmit={handleSubmit(createWithParams(create))}>
@@ -19,7 +27,7 @@ export default reduxForm({ form: 'forgotPassword' })(props => {
           name="email"
           label="E-mail"
           type="email"
-          validate={[required()]}
+          validate={[required(), email()]}
         />
         <Error>{error}</Error>
         <Button
