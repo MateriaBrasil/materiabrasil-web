fb8c13e531aba04ca7b9cf8b18f3c41962018390
Glauber Ramos <glauberamos@gmail.com>
2018-10-15 17:18:47 -0300

    Add styles

--- src/comparison/Chart.js
+++ src/comparison/Chart.js
@@ -4,6 +4,7 @@ import Icon from '@material-ui/icons/ExpandMore'
 import Drawer from '@material-ui/core/Drawer'
 import Paper from '@material-ui/core/Paper'
 import Chip from '@material-ui/core/Chip'
+import Grid from '@material-ui/core/Grid'
 import map from 'lodash/map'
 import reduce from 'lodash/reduce'
 import get from 'lodash/get'
@@ -39,31 +40,68 @@ export default ({ list, actions: { close, remove } }) => {
         <Icon style={{ marginLeft: 8 }} />
       </Button>
       <Paper>
-        <RadarChart width={320} height={320} outerRadius="50%" data={data}>
-          <PolarGrid />
-          <PolarAngleAxis dataKey="subject" />
-          <PolarRadiusAxis angle={45} domain={[0, 10]} />
-          {map(list, ({ id, name }, index) => (
-            <Radar
-              key={id}
-              name={name}
-              dataKey={id}
-              stroke={colors[index]}
-              fill={colors[index]}
-              fillOpacity={0.5}
-            />
-          ))}
-          <Legend />
-        </RadarChart>
+        <Grid
+          container
+          spacing={10}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Grid
+            item
+            lg={4}
+            md={4}
+            xs={12}
+            style={{ textAlign: 'center', marginBottom: 30 }}
+          >
+            <RadarChart
+              width={320}
+              height={320}
+              style={{ display: 'inline-flex' }}
+              outerRadius="50%"
+              data={data}
+            >
+              <PolarGrid />
+              <PolarAngleAxis dataKey="subject" />
+              <PolarRadiusAxis angle={45} domain={[0, 10]} />
+              {map(list, ({ id, name }, index) => (
+                <Radar
+                  key={id}
+                  name={name}
+                  dataKey={id}
+                  stroke={colors[index]}
+                  fill={colors[index]}
+                  fillOpacity={0.5}
+                />
+              ))}
+              <Legend />
+            </RadarChart>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            lg={3}
+            md={3}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {map(list, ({ id, name }, index) => (
+              <Chip
+                key={id}
+                label={name}
+                style={{ background: colors[index], marginBottom: 10 }}
+                onDelete={() => remove({ id })}
+                color="secondary"
+              />
+            ))}
+          </Grid>
+        </Grid>
       </Paper>
-      {map(list, ({ id, name }, index) => (
-        <Chip
-          key={id}
-          label={name}
-          onDelete={() => remove({ id })}
-          color="primary"
-        />
-      ))}
     </Drawer>
   )
 }
