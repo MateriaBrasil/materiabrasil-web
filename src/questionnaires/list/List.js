e4b6250afaca0aa44fc7a0221f725b0b998b8bac
Glauber Ramos <glauberamos@gmail.com>
2018-10-15 16:18:31 -0300

    Add dialog

--- src/questionnaires/list/List.js
+++ src/questionnaires/list/List.js
@@ -8,43 +8,46 @@ import RadioGroup from '@material-ui/core/RadioGroup'
 import Radio from '@material-ui/core/Radio'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Typography from '@material-ui/core/Typography'
+import Dialog from 'materials/Dialog'
 
-export default ({ list }) => (
-  <List>
-    {map(list, questionnaire => {
-      const { name, questions } = questionnaire
+export default props => (
+  <Dialog {...props} title="Responder questionários" callToAction="Salvar">
+    <List style={{ width: '100%' }}>
+      {map(props.list, questionnaire => {
+        const { name, questions } = questionnaire
 
-      return (
-        <ListItem key={name}>
-          <Typography variant="headline">{name}</Typography>
-          {map(questions, question => {
-            const { options, description } = question
+        return (
+          <ListItem key={name}>
+            <Typography variant="headline">{name}</Typography>
+            {map(questions, question => {
+              const { options, description } = question
 
-            return (
-              <FormGroup key={description}>
-                <Typography variant="subheading">{description}</Typography>
-                <FormControl component="fieldset">
-                  <RadioGroup
-                    key={description}
-                    aria-label="Questionnarie Options"
-                    name="questionnaire-options"
-                  >
-                    {map(options, option => (
-                      <FormControlLabel
-                        value={toString(option.id)}
-                        key={option.id}
-                        disabled
-                        control={<Radio />}
-                        label={option.description}
-                      />
-                    ))}
-                  </RadioGroup>
-                </FormControl>
-              </FormGroup>
-            )
-          })}
-        </ListItem>
-      )
-    })}
-  </List>
+              return (
+                <FormGroup key={description}>
+                  <Typography variant="subheading">{description}</Typography>
+                  <FormControl component="fieldset">
+                    <RadioGroup
+                      key={description}
+                      aria-label="Questionnarie Options"
+                      name="questionnaire-options"
+                    >
+                      {map(options, option => (
+                        <FormControlLabel
+                          value={toString(option.value)}
+                          key={option.value}
+                          disabled
+                          control={<Radio />}
+                          label={option.description}
+                        />
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                </FormGroup>
+              )
+            })}
+          </ListItem>
+        )
+      })}
+    </List>
+  </Dialog>
 )
