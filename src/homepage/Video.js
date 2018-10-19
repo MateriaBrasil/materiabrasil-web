e1dbbea309a34cebadede30948edf77978f8bebc
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-19 15:56:51 -0300

    Add Video.js

---
+++ src/homepage/Video.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default props => (
+  <iframe
+    src="https://player.vimeo.com/video/140946149"
+    frameBorder="0"
+    width="100%"
+    height="100%"
+    webkitallowfullscreen="true"
+    mozallowfullscreen="true"
+    allowFullScreen="true"
+    title="Vídeo de introdução ao Materia Brasil"
+    style={{
+      maxWidth: '960',
+      maxHeight: '540',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      border: 0,
+      width: 'auto',
+      height: 'auto',
+    }}
+  />
+)
