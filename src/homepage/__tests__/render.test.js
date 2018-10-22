51cd55568825d7bc50b88f40496545eec8966c68
Peterson Passos <peterson.jfp@gmail.com>
2018-10-22 10:32:53 -0300

    add link to '/materials' in homepage

--- src/homepage/__tests__/render.test.js
+++ src/homepage/__tests__/render.test.js
@@ -1,6 +1,11 @@
+import React from 'react'
 import renderer from 'react-test-renderer'
 
 import render from '../render'
+
+jest.mock('react-router-dom/Link', () => props => (
+  <div {...props}>{props.children}</div>
+))
 
 it('renders correctly', () => {
   const props = {}
--- src/homepage/render.js
+++ src/homepage/render.js
@@ -1,32 +1,40 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import Link from 'react-router-dom/Link'
+import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Grid from './Grid'
 import Video from './Video'
 
 export default props => routeProps => (
   <div style={{ overflow: 'hidden' }}>
-    <Fragment>
-      <Grid>
-        <Typography
-          variant="display2"
-          color="inherit"
-          className="text"
-          style={{
-            color: '#fff',
-            display: 'inline-block',
-            verticalAlign: 'middle',
-            fontWeight: 300,
-          }}
-        >
-          Na <span style={{ fontWeight: 400 }}>Materia</span>
-          Mundi, nós ajudamos indústrias e consumidores a implementar ações para
-          uma economia circular aplicada.
-        </Typography>
-      </Grid>
+    <Grid>
+      <Typography
+        variant="display2"
+        color="inherit"
+        className="text"
+        style={{
+          color: '#fff',
+          display: 'inline-block',
+          verticalAlign: 'middle',
+          fontWeight: 300,
+        }}
+      >
+        Na <span style={{ fontWeight: 400 }}>Materia</span>
+        Mundi, nós ajudamos indústrias e consumidores a implementar ações para
+        uma economia circular aplicada.
+      </Typography>
+    </Grid>
 
-      <Grid isVideo>
-        <Video />
-      </Grid>
-    </Fragment>
+    <Grid isVideo>
+      <Video />
+    </Grid>
+
+    <Grid>
+      <Link to={`/materials`} style={{ textDecoration: 'none' }}>
+        <Button variant="raised" color="default" size="large">
+          Explore
+        </Button>
+      </Link>
+    </Grid>
   </div>
 )
