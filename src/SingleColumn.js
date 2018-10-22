81484d32d0dd93927c36327d3cff36b99c45106a
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-22 18:19:05 -0300

    Alter SinglePage props

--- src/SingleColumn.js
+++ src/SingleColumn.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 
-export default ({ children }) => (
-  <Grid container spacing={32}>
+export default ({ children, noSpacing }) => (
+  <Grid container spacing={noSpacing ? 0 : 32}>
     <Grid item xs={12} sm={8} md={6} style={{ margin: '0 auto' }}>
       {children}
     </Grid>
