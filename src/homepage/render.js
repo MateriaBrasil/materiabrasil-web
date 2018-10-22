7b2bddf6083c508ad5ebcbda6d391b1a575be1e3
Peterson Passos <peterson.jfp@gmail.com>
2018-10-22 09:09:26 -0300

    remove homepage's horizontal scroll

--- src/homepage/render.js
+++ src/homepage/render.js
@@ -4,27 +4,29 @@ import Grid from './Grid'
 import Video from './Video'
 
 export default props => routeProps => (
-  <Fragment>
-    <Grid>
-      <Typography
-        variant="display2"
-        color="inherit"
-        className="text"
-        style={{
-          color: '#fff',
-          display: 'inline-block',
-          verticalAlign: 'middle',
-          fontWeight: 300,
-        }}
-      >
-        Na <span style={{ fontWeight: 400 }}>Materia</span>
-        Mundi, nós ajudamos indústrias e consumidores a implementar ações para
-        uma economia circular aplicada.
-      </Typography>
-    </Grid>
+  <div style={{ overflow: 'hidden' }}>
+    <Fragment>
+      <Grid>
+        <Typography
+          variant="display2"
+          color="inherit"
+          className="text"
+          style={{
+            color: '#fff',
+            display: 'inline-block',
+            verticalAlign: 'middle',
+            fontWeight: 300,
+          }}
+        >
+          Na <span style={{ fontWeight: 400 }}>Materia</span>
+          Mundi, nós ajudamos indústrias e consumidores a implementar ações para
+          uma economia circular aplicada.
+        </Typography>
+      </Grid>
 
-    <Grid isVideo>
-      <Video />
-    </Grid>
-  </Fragment>
+      <Grid isVideo>
+        <Video />
+      </Grid>
+    </Fragment>
+  </div>
 )
