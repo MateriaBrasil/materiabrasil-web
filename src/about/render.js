1745abb2ade59640b2be0470309feafb2659f073
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-23 15:44:13 -0300

    Remove unused items

--- src/about/render.js
+++ src/about/render.js
@@ -1,6 +1,4 @@
 import React, { Fragment } from 'react'
-import Link from 'react-router-dom/Link'
-import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 
 import Grid from '../InstitutionalPagesGrid'
@@ -115,41 +113,6 @@ export default props => routeProps => (
       >
         <strong>Direcionadores</strong>
       </Typography>
-    </Grid>
-
-    <Grid>
-      <Link to={`/materials`} style={{ textDecoration: 'none' }}>
-        <Button
-          variant="raised"
-          color="default"
-          size="large"
-          style={{
-            borderRadius: 7,
-            fontWeight: 100,
-            textTransform: 'lowercase',
-            fontSize: 40,
-            padding: '15px 50px',
-            background: '#fff',
-          }}
-        >
-          Explore
-        </Button>
-      </Link>
-      <p
-        style={{
-          color: '#fff',
-          position: 'absolute',
-          bottom: 20,
-          width: '100%',
-          textAlign: 'center',
-          fontFamily: 'roboto',
-        }}
-      >
-        Apoio: Instituto C&A; <br />
-        Parceiros: Good Tech Lab, Fab City Global, Materiom;
-        <br />
-        Membro: Sistema B Brasil e CE100 Global Ellen MacArthur Foundation.
-      </p>
     </Grid>
   </Fragment>
 )
