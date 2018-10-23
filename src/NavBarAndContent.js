896f245a0686e64fb03a11f11ec19a45a7ba8853
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-22 22:03:18 -0300

    Add src/NavBarAndContent.js

---
+++ src/NavBarAndContent.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+import Content from 'Content'
+import NavBar from './navBar/NavBar'
+
+export default props => (
+  <Fragment>
+    <NavBar {...props} />
+    <Content>{props.children}</Content>
+  </Fragment>
+)
