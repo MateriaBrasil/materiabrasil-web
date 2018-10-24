351c86fb46d0d2e80af612ff4e51cb548cf7f138
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-24 13:05:24 -0300

    Add imageStyle to detach items

---
+++ src/about/style/ImageStyle.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  grid: {
+    [theme.breakpoints.down('md')]: {
+      padding: '100px 0',
+    },
+    [theme.breakpoints.down('xs')]: {
+      padding: '50px 0',
+    },
+    position: 'relative',
+    padding: '100px 0',
+    margin: '0 auto',
+    overflow: 'hidden',
+    textAlign: 'center',
+  },
+})
+
+export default withStyles(styles)(props => (
+  <Grid item xs={12} className={props.classes.grid}>
+    <img
+      src="https://images.template.net/wp-content/uploads/2016/03/17133511/Vector-Infographic-Report-Template-Premium-Download.jpg"
+      alt="placeholder image"
+      style={{ maxWidth: '100%', padding: '3%' }}
+    />
+  </Grid>
+))
