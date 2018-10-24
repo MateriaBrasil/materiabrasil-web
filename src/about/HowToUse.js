4ffd412b607727bb6fc2eb709be0a2e6f56b4a63
Peterson Passos <peterson.jfp@gmail.com>
2018-10-23 23:01:27 -0300

    fix code climat

--- src/about/HowToUse.js
+++ src/about/HowToUse.js
@@ -7,17 +7,17 @@ import Video from '../Video'
 
 const styles = theme => ({
   grid: {
-    position: 'relative',
-    padding: '100px 0',
-    margin: '0 auto',
-    overflow: 'hidden',
-    textAlign: 'center',
     [theme.breakpoints.down('md')]: {
       padding: '100px 0',
     },
     [theme.breakpoints.down('xs')]: {
       padding: '50px 0',
     },
+    position: 'relative',
+    padding: '100px 0',
+    margin: '0 auto',
+    overflow: 'hidden',
+    textAlign: 'center',
   },
 })
 
