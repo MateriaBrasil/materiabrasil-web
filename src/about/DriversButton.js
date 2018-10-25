27a03a48e91553c54f0d432b59f8debe253fbe11
Peterson Passos <peterson.jfp@gmail.com>
2018-10-25 09:44:07 -0300

    detached button of the drivers session

---
+++ src/about/DriversButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import Link from 'react-router-dom/Link'
+
+export default props => (
+  <Link to={`/materials`} style={{ textDecoration: 'none', marginRight: 24 }}>
+    <Button
+      variant="contained"
+      color={props.color}
+      style={{
+        borderRadius: 7,
+        textTransform: 'lowercase',
+        fontSize: 30,
+        margin: 5,
+      }}
+    >
+      {props.text}
+    </Button>
+  </Link>
+)
--- src/about/DriversButtons.js
+++ src/about/DriversButtons.js
@@ -1,86 +1,15 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import { withStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
-import Link from 'react-router-dom/Link'
 
 import gridStyle from './gridStyle'
+import DriversButton from './DriversButton'
 
 export default withStyles(gridStyle)(props => (
   <Grid item xs={12} className={props.classes.grid}>
-    <Link to={`/materials`} style={{ textDecoration: 'none', marginRight: 24 }}>
-      <Button
-        variant="contained"
-        color="default"
-        style={{
-          borderRadius: 7,
-          fontWeight: '30px',
-          textTransform: 'lowercase',
-          fontSize: 30,
-          marginLeft: '5px',
-          marginRight: '5px',
-          marginTop: '5px',
-          marginBottom: '5px',
-        }}
-      >
-        Clickable 1
-      </Button>
-    </Link>
-
-    <Link to={`/materials`} style={{ textDecoration: 'none', marginRight: 24 }}>
-      <Button
-        variant="contained"
-        color="primary"
-        style={{
-          borderRadius: 7,
-          fontWeight: '30px',
-          textTransform: 'lowercase',
-          fontSize: 30,
-          marginLeft: '5px',
-          marginRight: '5px',
-          marginTop: '5px',
-          marginBottom: '5px',
-        }}
-      >
-        Clickable 2
-      </Button>
-    </Link>
-
-    <Link to={`/materials`} style={{ textDecoration: 'none', marginRight: 24 }}>
-      <Button
-        variant="contained"
-        color="secondary"
-        style={{
-          borderRadius: 7,
-          textTransform: 'lowercase',
-          fontSize: 30,
-          marginLeft: '5px',
-          marginRight: '5px',
-          marginTop: '5px',
-          marginBottom: '5px',
-        }}
-      >
-        Clickable 3
-      </Button>
-    </Link>
-
-    <Link to={`/materials`} style={{ textDecoration: 'none', marginRight: 24 }}>
-      <Button
-        variant="contained"
-        color="default"
-        style={{
-          borderRadius: 7,
-          fontWeight: '30px',
-          textTransform: 'lowercase',
-          fontSize: 30,
-          marginLeft: '5px',
-          marginRight: '5px',
-          marginTop: '5px',
-          marginBottom: '5px',
-        }}
-      >
-        Clickable 4
-      </Button>
-    </Link>
+    <DriversButton color="default" text="Clickable 1" />
+    <DriversButton color="primary" text="Clickable 2" />
+    <DriversButton color="secondary" text="Clickable 3" />
+    <DriversButton color="default" text="Clickable 4" />
   </Grid>
 ))
