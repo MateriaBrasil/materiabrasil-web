aff9043114f947648321e5ec5bc56dd546aab044
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-25 20:44:06 -0300

    Refactor DrivesListOfNames.js

--- src/about/DrivesListOfNames.js
+++ src/about/DrivesListOfNames.js
@@ -1,62 +1,29 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
+
+import DriversName from './DriversName'
 
 export default props => (
   <Grid
     item
-    xs={10}
+    xs={12}
     style={{
       position: 'relative',
       padding: '0 0',
       margin: '0 0',
     }}
   >
-    <Typography
-      variant="h4"
-      color="inherit"
-      padding="0 0"
-      style={{
-        color: '#fff',
-        display: 'inline-block',
-        verticalAlign: 'middle',
-        marginTop: '5%',
-        fontWeight: 300,
-        marginLeft: '50%',
-      }}
-    >
-      <ul>
-        <li>
-          <span>Aldo Ometto</span>
-        </li>
-        <li>
-          <span>Weber Amaral</span>
-        </li>
-        <li>
-          <span>Juliana Sardinha</span>
-        </li>
-        <li>
-          <span>Rosangela Sardinha</span>
-        </li>
-        <li>
-          <span>Luiza Maia de Castro</span>
-        </li>
-        <li>
-          <span>Isadora Ribeiro</span>
-        </li>
-        <li>
-          <span>Tomás de Lara</span>
-        </li>
-        <li>
-          <span>Manuela Yamada</span>
-        </li>
-        <li>
-          <span>Bruno Temer</span>
-        </li>
-        <li>
-          <span>Jéssica Galdino"</span>
-        </li>
-      </ul>
-    </Typography>
+    <ul style={{ listStyle: 'none', padding: 0 }}>
+      <DriversName>Aldo Ometto</DriversName>
+      <DriversName>Weber Amaral</DriversName>
+      <DriversName>Juliana Sardinha</DriversName>
+      <DriversName>Rosangela Sardinha</DriversName>
+      <DriversName>Luiza Maia de Castro</DriversName>
+      <DriversName>Isadora Ribeiro</DriversName>
+      <DriversName>Tomás de Lara</DriversName>
+      <DriversName>Manuela Yamada</DriversName>
+      <DriversName>Bruno Temer</DriversName>
+      <DriversName>Jéssica Galdino"</DriversName>
+    </ul>
   </Grid>
 )
