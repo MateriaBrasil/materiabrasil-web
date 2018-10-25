98e64dcfa6a01148f5dd08bd6995cbfce0bc6b4d
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-25 01:04:42 -0300

    Create about/Goal.js to detach objects in render

---
+++ src/about/Goal.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Session from './Session'
+import Title from './Title'
+import Paragraph from './Paragraph'
+
+export default () => (
+  <Session>
+    <Title>Objetivo</Title>
+    <Paragraph>
+      O objetivo da MateriaMundi oferecer acesso à informação de qualidade e
+      parâmetros para que seus usuários possam escolher alternativas
+      comercialmente disponíveis e socioambiental responsáveis para os desafios
+      do dia a dia, variando desde o que vestimos até como construímos nossas
+      casas.
+      <br />
+      Através da criação de uma comunidade ativa a plataforma permite a criação
+      de conhecimento compartilhado sobre ações práticas para uma forma de
+      projetar, produzir e consumir mais responsável com a sociedade e o meio
+      ambiente.
+    </Paragraph>
+  </Session>
+)
