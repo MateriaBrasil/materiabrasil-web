4de57ae6264b18456608180ca444e9713e471c10
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-26 16:01:15 -0300

    Alter link images

--- src/about/board/membersList.js
+++ src/about/board/membersList.js
@@ -3,36 +3,43 @@ export default [
     id: 1,
     name: 'Margarida Lunetta',
     image:
-      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Margarida.png',
+      'https://s3.amazonaws.com/materiamundi-us/static/conselho/margarida_lunetta.png',
     job: 'Gestora do programa Transformação Circular da Fundação C&A',
   },
   {
     id: 2,
     name: 'Arnoud Passenier',
     image:
-      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Arnoud.png',
+      'https://s3.amazonaws.com/materiamundi-us/static/conselho/arnoud_passenier.png',
     job: 'Gestor Sênior do programa de Economia Circular do governo da Holanda',
   },
   {
     id: 3,
     name: 'Luisa Santiago',
     image:
-      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Luisa.png',
+      'https://s3.amazonaws.com/materiamundi-us/static/conselho/luisa_santiago.png',
     job: 'Diretora Executiva Ellen MacArthur Foundation Brasil',
   },
   {
     id: 4,
     name: 'Tomás de Lara',
     image:
-      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Tom%C3%A1s.png',
+      'https://s3.amazonaws.com/materiamundi-us/static/conselho/tomas_de_lara.jpg',
     job: 'Lead Cidades + B Global Sistema B Brasil',
   },
   {
     id: 5,
     name: 'Taciana Abreu',
     image:
-      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Taciana.png',
+      'https://s3.amazonaws.com/materiamundi-us/static/conselho/taciana_abreu.png',
     job:
       'Articuladora Yunus Negócios Sociais Brasil e Head de Marketing Farm Rio',
+  },
+  {
+    id: 6,
+    name: 'Bernardo Ferracioli',
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/conselho/bernardo_ferracioli.png',
+    job: 'texto pendente',
   },
 ]
