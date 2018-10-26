6502b1bcfbdb242314863fe58ee233ee1a856fe3
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-26 15:45:30 -0300

    Recover lost render.test.js

---
+++ src/about/__tests__/render.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import render from '../render'
+
+jest.mock('react-router-dom/Link', () => props => (
+  <div {...props}>Link - {props.children}</div>
+))
+
+jest.mock('../../navBar/NavBar', () => props => <div {...props}>NavBar</div>)
+
+it('renders correctly', () => {
+  const props = {}
+  const routeProps = {}
+  const tree = renderer.create(render(props)(routeProps)).toJSON()
+  expect(tree).toMatchSnapshot()
+})
