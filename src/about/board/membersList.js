fce2decf956f35eea128b495e0096aeb38ca16f7
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-26 14:31:59 -0300

    Change images repository

--- src/about/board/membersList.js
+++ src/about/board/membersList.js
@@ -3,35 +3,35 @@ export default [
     id: 1,
     name: 'Margarida Lunetta',
     image:
-      'https://lh6.googleusercontent.com/BBN6N20unckTJoeEFYB3qT93EbTlmprR0fi5WYNVTVUoZ-CtOiSUcttA5CIICRhKTOvUU1C4643nyw=w1920-h1008',
+      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Margarida.png',
     job: 'Gestora do programa Transformação Circular da Fundação C&A',
   },
   {
     id: 2,
     name: 'Arnoud Passenier',
     image:
-      'https://lh6.googleusercontent.com/L49lKzovMGDESQh8OOvaZdBU--fC84U7AKHmtylrnnrYcK5Rp81X8mTJneY6zTSVXs9AI5F4L_WHvpU93RyW=w1920-h1008',
+      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Arnoud.png',
     job: 'Gestor Sênior do programa de Economia Circular do governo da Holanda',
   },
   {
     id: 3,
     name: 'Luisa Santiago',
     image:
-      'https://lh6.googleusercontent.com/LnSC7MuQGVmk9leH1mC0jHOuXidEQSVxALSpxNKqmAyzaL5ALHivY0w3e3tcGKBllKLMlDxx8JzEM7cxk1iE=w1920-h1008',
+      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Luisa.png',
     job: 'Diretora Executiva Ellen MacArthur Foundation Brasil',
   },
   {
     id: 4,
     name: 'Tomás de Lara',
     image:
-      'https://lh6.googleusercontent.com/hyBHnU0aZbM_nOyyBFjaidpbAFDDxhWqib21PZk2ryORM3LjoIOj58KXhD8Ro0R2XRCLHsxVDdT0LzbtdYP0=w1920-h1008',
+      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Tom%C3%A1s.png',
     job: 'Lead Cidades + B Global Sistema B Brasil',
   },
   {
     id: 5,
     name: 'Taciana Abreu',
     image:
-      'https://lh3.googleusercontent.com/jAnSyqgv1wLHPHXMUuikVjT26Up4wrPwVfrt-bmTBj6Q4BuZQyAfQWWx_r35LklfcE87Tmb4ZnoKElcm35sg=w1920-h1008',
+      'https://s3.amazonaws.com/materiamundi-us/static/driversCrew/Taciana.png',
     job:
       'Articuladora Yunus Negócios Sociais Brasil e Head de Marketing Farm Rio',
   },
