04ed63db5f88cee275b484a6f5ab81c82a545eb6
Peterson Passos <peterson.jfp@gmail.com>
2018-10-26 11:54:52 -0300

    add crew photos

--- src/about/crewMembersList.js
+++ src/about/crewMembersList.js
@@ -1,4 +1,3 @@
-const temporaryImage = 'https://via.placeholder.com/200x200'
 const temporaryJob = 'Job do integrante'
 const temporaryDescription =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in congue dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst.'
@@ -7,70 +6,80 @@ export default [
   {
     id: 1,
     name: 'Manuela Yamada',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Manuela_Yamada.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 2,
     name: 'Bernardo Ferracioli',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Bernardo_Ferracioli.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 3,
     name: 'Bruno Temer',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Bruno_Temer.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 4,
     name: 'Pedro Themoteo',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Pedro_Themoteo.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 5,
     name: 'Daniele Valente',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Daniele_Valente.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 6,
     name: 'Elisa Ferreira',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Elisa_Ferreira.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 7,
     name: 'Victor Lanari',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Victor_Lanari.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 8,
     name: 'Dahyna Ribeiro',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Dahyna_Irribaren.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 9,
     name: 'Bernardo Mattos',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Bernardo_Mattos.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
   {
     id: 10,
     name: 'Seasoned',
-    image: temporaryImage,
+    image:
+      'https://s3.amazonaws.com/materiamundi-us/static/equipe/Seasoned.jpg',
     job: temporaryJob,
     description: temporaryDescription,
   },
