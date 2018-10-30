528c4e0b3133390f1dd6eac954b11f7b7d0e0508
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-30 14:18:31 -0300

    Alter ResponsiveGrid

--- src/about/ResponsiveGrid.js
+++ src/about/ResponsiveGrid.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 
 export default ({ style, className, children }) => (
-  <Grid item xs={12} sm={8} md={6} className={className} style={style}>
+  <Grid item xs={12} sm={8} md={8} className={className} style={style}>
     {children}
   </Grid>
 )
