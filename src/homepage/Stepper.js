cd547608ac88a64ddf3a51071140f13dc1f774f3
Peterson Passos <peterson.jfp@gmail.com>
2018-10-31 16:40:55 -0300

    add stepper

---
+++ src/homepage/Stepper.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Stepper from '@material-ui/core/Stepper'
+import Step from '@material-ui/core/Step'
+import StepLabel from '@material-ui/core/StepLabel'
+import StepContent from '@material-ui/core/StepContent'
+import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+const styles = theme => ({
+  root: {
+    width: '214px',
+    background: 'transparent',
+    position: 'fixed',
+    right: 0,
+    top: 200,
+    boxShadow: 'none'
+  },
+  label: {
+      color: "#fff"
+  }
+})
+
+function getSteps() {
+  return ['Select campaign settings', 'Create an ad group', 'Create an ad']
+}
+
+class VerticalLinearStepper extends React.Component {
+  state = {
+    activeStep: 0,
+  }
+
+  handleStep = step => () => {
+    this.setState({
+      activeStep: step,
+    });
+  };
+
+  render() {
+    const { activeStep } = this.state
+
+    return (
+      <Stepper activeStep={activeStep} orientation="vertical" className={this.props.classes.root}>
+        <Step key="1" onClick={this.handleStep(0)}>
+          <StepLabel className={this.props.classes.label}>Step 1</StepLabel>
+        </Step>
+        <Step key="2" onClick={this.handleStep(1)}>
+          <StepLabel className={this.props.classes.label}>Step 2</StepLabel>
+        </Step>
+        <Step key="3" onClick={this.handleStep(2)}>
+          <StepLabel className={this.props.classes.label}>Step 3</StepLabel>
+        </Step>
+      </Stepper>
+    )
+  }
+}
+
+VerticalLinearStepper.propTypes = {
+  classes: PropTypes.object,
+}
+
+export default withStyles(styles)(VerticalLinearStepper)
--- src/homepage/render.js
+++ src/homepage/render.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 import Link from 'react-router-dom/Link'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+
+import Stepper from './Stepper'
 import Grid from '../InstitutionalPagesGrid'
 import Video from '../Video'
 import NavBar from '../navBar/NavBar'
@@ -9,65 +11,7 @@ import Colors from '../Colors'
 
 export default props => routeProps => (
   <Fragment>
-    <NavBar />
-    <Grid>
-      <Typography
-        variant="h3"
-        color="inherit"
-        className="text"
-        style={{
-          color: Colors.white,
-          display: 'inline-block',
-          verticalAlign: 'middle',
-          fontWeight: 300,
-          padding: '0 20px',
-        }}
-      >
-        Na <span style={{ fontWeight: 400 }}>Materia</span>
-        Mundi, nós ajudamos indústrias e consumidores a implementar ações para
-        uma economia circular aplicada.
-      </Typography>
-    </Grid>
-
-    <Grid isVideo>
-      <Video />
-    </Grid>
-
-    <Grid>
-      <Link to={`/materials`} style={{ textDecoration: 'none' }}>
-        <Button
-          variant="contained"
-          color="default"
-          size="large"
-          style={{
-            borderRadius: 7,
-            fontWeight: 100,
-            textTransform: 'lowercase',
-            fontSize: 40,
-            padding: '15px 50px',
-            background: '#fff',
-            marginBottom: 50,
-          }}
-        >
-          Explore
-        </Button>
-      </Link>
-      <Typography
-        style={{
-          color: Colors.white,
-          position: 'absolute',
-          bottom: 20,
-          width: '100%',
-          textAlign: 'center',
-          fontFamily: 'roboto',
-          padding: '0 20px',
-        }}
-      >
-        Apoio: Instituto C&A; <br />
-        Parceiros: Good Tech Lab, Fab City Global, Materiom;
-        <br />
-        Membro: Sistema B Brasil e CE100 Global Ellen MacArthur Foundation.
-      </Typography>
-    </Grid>
+    <NavBar {...props} isInstitutionalPage="true" />
+    <Stepper {...props} />
   </Fragment>
 )
