4cb992577edc97e0349ee4e6316aa62fccb394c7
Rodrigo Avellar <roavellarm@gmail.com>
2018-10-31 16:44:39 -0300

    Wrap image links to ButtonStyle for hover effect

--- src/about/drivers/Symbol.js
+++ src/about/drivers/Symbol.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import Dialog from '@material-ui/core/Dialog'
+
 import Paragraph from '../Paragraph'
 import Title from '../Title'
+import ButtonStyle from './ButtonStyle'
 
 export default class extends Component {
   state = {
@@ -21,7 +23,10 @@ export default class extends Component {
   render() {
     return (
       <Fragment>
-        <img src={this.props.imageUrl} onClick={this.handleClickOpen} />
+        <ButtonStyle>
+          <img src={this.props.imageUrl} onClick={this.handleClickOpen} />
+        </ButtonStyle>
+
         <Dialog
           open={this.state.open}
           aria-labelledby="responsive-dialog-title"
