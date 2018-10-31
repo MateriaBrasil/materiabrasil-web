ddaf05b290ca72bd706542a538e5106394966cc0
Peterson Passos <peterson.jfp@gmail.com>
2018-10-31 10:58:30 -0300

    remove unnecessary files

--- src/materials/FileDownloadIcon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CloudDownload from '@material-ui/icons/CloudDownload'
-import IconButtonWithTooltip from './IconButtonWithTooltip'
-
-export default ({ url, style, tooltip }) => (
-  <IconButtonWithTooltip tooltip={tooltip} url={url} style={style}>
-    <CloudDownload color="inherit" />
-  </IconButtonWithTooltip>
-)
--- src/materials/TechnicalSpecificationIcon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import InsertChart from '@material-ui/icons/InsertChart'
-import IconButtonWithTooltip from './IconButtonWithTooltip'
-
-export default ({ url, style, tooltip }) => (
-  <IconButtonWithTooltip tooltip={tooltip} url={url} style={style}>
-    <InsertChart color="inherit" />
-  </IconButtonWithTooltip>
-)
