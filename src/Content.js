e00018406765ff92261d01c0841e0d91db36e529
Rodrigo Avellar <roavellarm@gmail.com>
2018-11-01 10:34:09 -0300

    Add paddingTop to root content container to avoid navbar bug

--- src/Content.js
+++ src/Content.js
@@ -2,4 +2,8 @@ import React from 'react'
 
 import './Content.css'
 
-export default ({ children }) => <div className="content">{children}</div>
+export default ({ children }) => (
+  <div className="content" style={{ paddingTop: '140px' }}>
+    {children}
+  </div>
+)
