4e03d2a4b87d94a678501b55917aedbd6090df79
Rodrigo Avellar <roavellarm@gmail.com>
2018-11-01 09:49:49 -0300

    Adjust symbol style

--- src/about/drivers/Symbol.js
+++ src/about/drivers/Symbol.js
@@ -1,8 +1,9 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
 import Dialog from '@material-ui/core/Dialog'
+import Typography from '@material-ui/core/Typography'
 
-import Paragraph from '../Paragraph'
-import Title from '../Title'
+import Colors from '../../Colors'
 import ButtonStyle from './ButtonStyle'
 
 export default class extends Component {
@@ -22,20 +23,42 @@ export default class extends Component {
 
   render() {
     return (
-      <Fragment>
-        <ButtonStyle>
+      <Grid item xs={12} sm={6} md={4} lg={3} style={{ padding: 20 }}>
+        <ButtonStyle
+          style={{
+            display: 'block',
+            height: 200,
+          }}
+        >
           <img src={this.props.imageUrl} onClick={this.handleClickOpen} />
         </ButtonStyle>
+        <Typography
+          variant="h5"
+          style={{
+            textAlign: 'center',
+            color: Colors.white,
+            fontWeight: 500,
+          }}
+        >
+          {this.props.title}
+        </Typography>
+        <Typography
+          variant="h6"
+          style={{
+            textAlign: 'center',
+            color: Colors.white,
+            fontWeight: 300,
+          }}
+        >
+          {this.props.desc}
+        </Typography>
 
         <Dialog
           open={this.state.open}
           aria-labelledby="responsive-dialog-title"
           onClose={this.handleClose}
-        >
-          <Title color>{this.props.title}</Title>
-          <Paragraph color>{this.props.desc}</Paragraph>
-        </Dialog>
-      </Fragment>
+        />
+      </Grid>
     )
   }
 }
