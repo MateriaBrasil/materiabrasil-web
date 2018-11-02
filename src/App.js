95d49fa4aa3c8b196cd40933761b0bfd744e3399
felipefreitag <ffvargas@gmail.com>
2018-11-02 13:22:07 -0300

    Fix linter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import { Provider as CroodsProvider } from 'croods'
-import { MuiThemeProvider } from '@material-ui/core/styles'
 
 import Auth from 'auth/Auth'
-import headers from 'auth/headers'
-import afterSuccess from 'auth/afterSuccess'
 import Comparison from 'comparison/Comparison'
 import Snackbar from 'snackbar/Snackbar'
-import store from 'store/store'
-
-import renderLoading from './renderLoading'
-import renderError from './renderError'
 
 import Screen from './Screen'
-import theme from './theme'
-
 import Providers from './Providers'
 
 export default class extends Component {
