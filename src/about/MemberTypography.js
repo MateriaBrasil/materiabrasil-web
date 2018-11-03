88a4c18160d398b0f10e7ab104af3052e8b2ec8f
felipefreitag <ffvargas@gmail.com>
2018-11-03 10:44:08 -0300

    Fix tests

--- src/about/MemberTypography.js
+++ src/about/MemberTypography.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Colors from '../Colors'
 
-export default ({ style, ...props }) => (
+export default ({ style, color, ...props }) => (
   <Typography
     {...props}
     style={{
       textAlign: 'center',
-      color: props.color ? props.color : Colors.white,
+      color: color ? color : Colors.white,
       fontWeight: 500,
       ...style,
     }}
