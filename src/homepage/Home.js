b07ba0cbf1ac884f4d3b7d1a8b96828a59f126f7
Daniel Weinmann <danielweinmann@gmail.com>
2018-11-03 12:34:54 -0200

    Temporarily remove stepper from home

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -1,7 +1,5 @@
 import React, { Fragment } from 'react'
-import Media from 'react-media'
 import Sections from './Sections'
-import Stepper from './Stepper'
 import handleScroll from './handleScroll'
 import NavBar from '../navBar/NavBar'
 
@@ -13,13 +11,9 @@ export default class extends React.Component {
   }
 
   render() {
-    const { activeStep } = this.state
     return (
       <Fragment>
         <NavBar />
-        <Media query="(min-width: 960px)">
-          {matches => (matches ? <Stepper activeStep={activeStep} /> : null)}
-        </Media>
         <Sections handleScroll={this.handleScroll} />
       </Fragment>
     )
