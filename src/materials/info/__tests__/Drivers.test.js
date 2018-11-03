b7bd31c9eb855e93362712f73b0e585679c8278c
felipefreitag <ffvargas@gmail.com>
2018-11-03 16:40:56 -0300

    Rename drivers

--- src/materials/info/__tests__/Drivers.test.js
+++ src/materials/info/__tests__/Drivers.test.js
@@ -5,10 +5,10 @@ import Drivers from '../Drivers'
 
 it('renders correctly', () => {
   const props = {
-    firstDriver: 50,
-    secondDriver: 50,
-    thirdDriver: 50,
-    fourthDriver: 50,
+    materialityDriver: 50,
+    manufactureDriver: 50,
+    managementDriver: 50,
+    socialDriver: 50,
   }
   const tree = renderer.create(<Drivers {...props} />).toJSON()
   expect(tree).toMatchSnapshot()
