a0c7dfe43c0c55a0ae934714e220d2074fdfd941
Glauber Ramos <glauberamos@gmail.com>
2018-11-06 11:30:41 -0200

    Refactor components

--- src/about/HowToUse.js
+++ src/about/HowToUse.js
@@ -1,30 +1,15 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
 
 import SectionTitle from 'homepage/SectionTitle'
 import Section from './Section'
+import Infograph from './Infograph'
 
 export default props => (
   <div style={{ position: 'relative' }}>
     <SectionTitle image="/images/polygon-2.png">Como usar</SectionTitle>
     <Section id="how_to_use">
-      <Grid item xs={12} style={{ textAlign: 'center' }}>
-        <Typography variant="h4">Usuário</Typography>
-        <img
-          src="images/infograph1.png"
-          alt=""
-          style={{ maxWidth: '800px', padding: '3%' }}
-        />
-      </Grid>
-      <Grid item xs={12} style={{ textAlign: 'center' }}>
-        <Typography variant="h4">Fornecedor</Typography>
-        <img
-          src="images/infograph2.png"
-          alt=""
-          style={{ maxWidth: '800px', padding: '3%' }}
-        />
-      </Grid>
+      <Infograph title="Fornecedor" url="images/infograph1.png" />
+      <Infograph title="Usuário" url="images/infograph2.png" />
     </Section>
   </div>
 )
