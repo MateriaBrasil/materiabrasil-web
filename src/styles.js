93b95fabc978e899da0a100b421d4a4bb5e8b27e
Rodrigo Avellar <roavellarm@gmail.com>
2018-11-06 11:58:04 -0200

    Create styles.js at src

---
+++ src/styles.js
@@ -0,0 +1,11 @@
+export default theme => ({
+  navbarPadding: {
+    paddingTop: '124px',
+    [theme.breakpoints.down('md')]: {
+      paddingTop: '80px',
+    },
+  },
+  institutional: {
+    padding: '0',
+  },
+})
