e1cb3dfc33b2f460a31814c8e626a167a735c8b1
Peterson Passos <peterson.jfp@gmail.com>
2018-11-07 10:30:03 -0200

    update charts range

--- src/comparison/Chart.js
+++ src/comparison/Chart.js
@@ -67,7 +67,7 @@ export default withStyles(chartsStyle)(
                 >
                   <PolarGrid />
                   <PolarAngleAxis dataKey="subject" />
-                  <PolarRadiusAxis angle={45} domain={[0, 10]} />
+                  <PolarRadiusAxis angle={45} domain={[0, 4]} />
                   {map(list, ({ id, name }, index) => (
                     <Radar
                       key={id}
