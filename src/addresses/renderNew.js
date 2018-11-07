3e4a0b480d4802abce35a634defb7dd83a0fc940
Glauber Ramos <glauberamos@gmail.com>
2018-11-07 16:43:21 -0200

    Refactor address to be a dialog

--- src/addresses/renderNew.js
+++ src/addresses/renderNew.js
@@ -2,19 +2,16 @@ import React from 'react'
 
 import Form from './Form'
 import createWithParams from './createWithParams'
-import Card from './Card'
 
 export default props => ({ create, creating, error }) => {
-  const { id } = props
+  const { id } = props.match.params
 
   return (
-    <Card title="Adicionar endereço">
-      <Form
-        {...props}
-        onSubmit={createWithParams(create, id)}
-        creating={creating}
-        createError={error}
-      />
-    </Card>
+    <Form
+      {...props}
+      onSubmit={createWithParams(create, id)}
+      creating={creating}
+      createError={error}
+    />
   )
 }
