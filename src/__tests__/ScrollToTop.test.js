e86fe2acefed764835b49f4bd42b7c94c6c45046
Peterson Passos <peterson.jfp@gmail.com>
2018-11-08 11:01:46 -0200

    fix lint

--- src/__tests__/ScrollToTop.test.js
+++ src/__tests__/ScrollToTop.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import ScrollToTop from '../ScrollToTop'
 import { MemoryRouter } from 'react-router'
+import ScrollToTop from '../ScrollToTop'
 
 jest.mock('react-router-dom', () => ({ withRouter: Component => Component }))
 
