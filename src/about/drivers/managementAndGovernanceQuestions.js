dae385eed2afa82c9d447c19e0f6630eb1675ce3
Peterson Passos <peterson.jfp@gmail.com>
2018-11-08 19:00:05 -0200

    add questions to project

---
+++ src/about/drivers/managementAndGovernanceQuestions.js
@@ -0,0 +1,138 @@
+export default [
+  {
+    id: 1,
+    question: '1 - Qual a proporção de mulheres na empresa?',
+  },
+  {
+    id: 2,
+    question:
+      '2 - Qual a proporção de mulheres nos cargos de decisão na empresa?',
+  },
+  {
+    id: 3,
+    question:
+      '3 - Sobre o processo de recrutamento e seleção: a empresa possui políticas de equidade de gênero?',
+  },
+  {
+    id: 4,
+    question:
+      '4 - A empresa possui alguma instância responsável por promover e supervisionar a implementação de ações de promoção da equidade de gênero?',
+  },
+  {
+    id: 5,
+    question:
+      '5 - A empresa possui práticas ou políticas para combater o preconceito e a violência de gênero?',
+  },
+  {
+    id: 6,
+    question:
+      '6 - A empresa possui práticas ou políticas para promover e garantir a liberdade, equidade e diversidade sexual?',
+  },
+  {
+    id: 7,
+    question: '7 - Qual a proporção de mulheres negras na empresa?',
+  },
+  {
+    id: 8,
+    question:
+      '8 - Qual a proporção de mulheres negras nos cargos de decisão na empresa?',
+  },
+  {
+    id: 9,
+    question:
+      '9 - Qual a proporção de pessoas com deficiência (PCD) na empresa?',
+  },
+  {
+    id: 10,
+    question:
+      '10 - A empresa promove ações e campanhas internas de sensibilização para fomentar um ambiente inclusivo onde as pessoas com deficiência são envolvidas e participam do diálogo das ações inclusivas?',
+  },
+  {
+    id: 11,
+    question:
+      '11 - Em relação à licença-maternidade e paternidade, qual a política da empresa?',
+  },
+  {
+    id: 12,
+    question: '12 - A empresa oferece plano de saúde para os empregados?',
+  },
+  {
+    id: 13,
+    question:
+      '13 - A empresa oferece benefícios de alimentação para o empregado?',
+  },
+  {
+    id: 14,
+    question: '14 - Com relação ao desenvolvimento técnico profissional',
+  },
+  {
+    id: 15,
+    question: '15 - Com relação ao desenvolvimento comportamental',
+  },
+  {
+    id: 16,
+    question:
+      '16 - A empresa possui processo de avaliação do desempenho dos empregados?',
+  },
+  {
+    id: 17,
+    question:
+      '17 - Os funcionários tem um canal de feedback e sugestões de melhorias?',
+  },
+  {
+    id: 18,
+    question: '18 - A empresa possui registro de funcionários no e-social?',
+  },
+  {
+    id: 19,
+    question: '19 - A empresa possui PCMSO e PPRA atualizados?',
+  },
+  {
+    id: 20,
+    question:
+      '20 - A empresa realiza e mantém os atestados de saúde ocupacional atualizados?',
+  },
+  {
+    id: 21,
+    question:
+      '21 - A empresa oferece EPIs adequados para a realização das atividades para todos os funcionários?',
+  },
+  {
+    id: 22,
+    question:
+      '22 - Os equipamentos e mobiliários utilizados pelos funcionários são analisados ergonomicamente?',
+  },
+  {
+    id: 23,
+    question: '23 - Há política de gestão de resíduos dentro da empresa?',
+  },
+  {
+    id: 24,
+    question: '24 - Qual a proporção de negros na empresa?',
+  },
+  {
+    id: 25,
+    question:
+      '25 - Qual a proporção de negros nos cargos de decisão na empresa?',
+  },
+  {
+    id: 26,
+    question:
+      '26 - Sobre o processo de recrutamento e seleção: a empresa possui políticas de equidade de raça?',
+  },
+  {
+    id: 27,
+    question:
+      '27 - A empresa possui alguma instância responsável por promover e supervisionar a implementação de ações de promoção da diversidade e equidade de raça?',
+  },
+  {
+    id: 28,
+    question:
+      '28 - A empresa possui práticas ou políticas para combater o preconceito e a violência de raça?',
+  },
+  {
+    id: 29,
+    question:
+      '29 - Qual o multiplicador de salário entre o menor e o maior salário da sua organização? política de cargos e salários',
+  },
+]
