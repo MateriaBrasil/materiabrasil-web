c6d019a9221885fec9108b0606d0f411904dd876
Rodrigo Avellar <roavellarm@gmail.com>
2018-11-10 21:43:04 -0200

    Add reducer.test.js

---
+++ src/snackbar/__tests__/reducer.test.js
@@ -0,0 +1,32 @@
+import reducer from '../reducer'
+
+describe('without action type', () => {
+  const state = { foo: 'bar' }
+
+  it('returns as expected', () => {
+    expect(reducer(state, null)).toMatchObject(state)
+  })
+})
+
+describe('with @snackbar/SET_MESSAGE action type', () => {
+  const state = { foo: 'bar' }
+  const action = { type: '@snackbar/SET_MESSAGE' }
+
+  it('returns as expected', () => {
+    expect(reducer(state, action)).toMatchObject({
+      ...state,
+      message: action.describe,
+    })
+  })
+})
+
+describe('with @snackbar/CLEAR_MESSAGE action type', () => {
+  const state = { foo: 'bar' }
+  const action = { type: '@snackbar/CLEAR_MESSAGE' }
+
+  it('returns as expected', () => {
+    expect(reducer(state)).toMatchObject({
+      ...state,
+    })
+  })
+})
