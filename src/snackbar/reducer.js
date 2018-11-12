12919f84255d6008c99045fb0afca7c3e2fa9bc8
Rodrigo Avellar <roavellarm@gmail.com>
2018-11-12 00:57:54 -0200

    Return reducer to initioal state (action on line 14)

--- src/snackbar/reducer.js
+++ src/snackbar/reducer.js
@@ -11,7 +11,7 @@ const reducers = {
     ...state,
     message: action.message,
   }),
-  CLEAR_MESSAGE: state => ({
+  CLEAR_MESSAGE: (state, action) => ({
     ...state,
     message: null,
   }),
