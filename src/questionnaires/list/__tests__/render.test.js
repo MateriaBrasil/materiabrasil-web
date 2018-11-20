f575e98d06adf335805497a45003195b19dc4edf
Glauber Ramos <glauberamos@gmail.com>
2018-11-20 17:56:19 -0200

    Fix test

--- src/questionnaires/list/__tests__/render.test.js
+++ src/questionnaires/list/__tests__/render.test.js
@@ -3,10 +3,14 @@ import renderer from 'react-test-renderer'
 
 import render from '../render'
 
-jest.mock('../List', () => props => <div {...props}>List</div>)
+jest.mock('croods', () => ({
+  List: props => <div {...props}>{props.children}</div>,
+}))
 
 it('renders correctly', () => {
-  const Component = render({ foo: 'bar' })
+  const routeProps = { match: { params: { id: 123 } } }
+  const Component = render({ foo: 'bar', ...routeProps })
+
   const tree = renderer.create(<Component bar="foo" />).toJSON()
   expect(tree).toMatchSnapshot()
 })
