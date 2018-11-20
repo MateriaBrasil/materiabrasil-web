da5f05ce4d9c5120bacee4e29a139a7ed8e48fce
Glauber Ramos <glauberamos@gmail.com>
2018-11-20 18:58:59 -0200

    Refactor list

--- src/questionnaires/list/List.js
+++ src/questionnaires/list/List.js
@@ -1,71 +1,16 @@
 import React from 'react'
 import map from 'lodash/map'
 import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import FormGroup from '@material-ui/core/FormGroup'
-import FormControl from '@material-ui/core/FormControl'
-import RadioGroup from '@material-ui/core/RadioGroup'
-import Radio from '@material-ui/core/Radio'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Typography from '@material-ui/core/Typography'
 import Dialog from 'materials/Dialog'
-import createWithParams from './createWithParams'
-import findAnswer from './findAnswer'
-import findOption from './findOption'
+import renderQuestionnaire from './renderQuestionnaire'
 
 export default props => {
-  const { questionnaires, questionnairesAnswers, create } = props
+  const { questionnaires } = props
 
   return (
     <Dialog {...props} title="Responder questionários" callToAction="Salvar">
       <List style={{ width: '100%' }}>
-        {map(questionnaires, questionnaire => (
-          <ListItem
-            key={questionnaire.name}
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-            }}
-          >
-            <Typography variant="h5">{questionnaire.name}</Typography>
-            {map(questionnaire.questions, question => {
-              const { options, description } = question
-
-              const answer = findAnswer(questionnairesAnswers, question)
-              const answerOption = findOption(answer, question)
-
-              return (
-                <FormGroup key={description}>
-                  <Typography variant="subtitle1">{description}</Typography>
-                  <FormControl component="fieldset">
-                    <RadioGroup
-                      key={description}
-                      aria-label="Questionnarie Options"
-                      name="questionnaire-options"
-                      value={answerOption ? answerOption.value : undefined}
-                    >
-                      {map(options, option => (
-                        <FormControlLabel
-                          value={option.value}
-                          key={option.value}
-                          onChange={createWithParams(
-                            create,
-                            props.match.params.id,
-                            option.id,
-                            question.id,
-                          )}
-                          control={<Radio />}
-                          label={option.description}
-                        />
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                </FormGroup>
-              )
-            })}
-          </ListItem>
-        ))}
+        {map(questionnaires, renderQuestionnaire(props))}
       </List>
     </Dialog>
   )
---
+++ src/questionnaires/list/renderQuestion.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import map from 'lodash/map'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControl from '@material-ui/core/FormControl'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Typography from '@material-ui/core/Typography'
+import renderOption from './renderOption'
+import findAnswer from './findAnswer'
+import findOption from './findOption'
+
+export default props => question => {
+  const { options, description } = question
+
+  const answer = findAnswer(props.questionnairesAnswers, question)
+  const answerOption = findOption(answer, question)
+
+  return (
+    <FormGroup key={description}>
+      <Typography variant="subtitle1">{description}</Typography>
+      <FormControl component="fieldset">
+        <RadioGroup
+          key={description}
+          aria-label="Questionnarie Options"
+          name="questionnaire-options"
+          value={answerOption ? answerOption.value : undefined}
+        >
+          {map(options, renderOption(props, question))}
+        </RadioGroup>
+      </FormControl>
+    </FormGroup>
+  )
+}
