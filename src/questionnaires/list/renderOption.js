697aa5c744993c677d42e71ad3bc87bb1685d536
Glauber Ramos <glauberamos@gmail.com>
2018-11-26 17:41:26 -0200

    Fix console warning

--- src/questionnaires/list/renderOption.js
+++ src/questionnaires/list/renderOption.js
@@ -3,18 +3,22 @@ import Radio from '@material-ui/core/Radio'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import createWithParams from './createWithParams'
 
-export default (props, question) => option => (
-  <FormControlLabel
-    value={option.value}
-    key={option.value}
-    onChange={createWithParams({
-      create: props.create,
-      aboutId: props.match.params.id,
-      aboutType: props.aboutType,
-      optionId: option.id,
-      questionId: question.id,
-    })}
-    control={<Radio />}
-    label={option.description}
-  />
-)
+export default (props, question) => option => {
+  const value = option.value ? option.value.toString() : ''
+
+  return (
+    <FormControlLabel
+      value={value}
+      key={option.value}
+      onChange={createWithParams({
+        create: props.create,
+        aboutId: props.match.params.id,
+        aboutType: props.aboutType,
+        optionId: option.id,
+        questionId: question.id,
+      })}
+      control={<Radio />}
+      label={option.description}
+    />
+  )
+}
