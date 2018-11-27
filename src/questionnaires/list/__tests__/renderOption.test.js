2d1a6a79b54d6004e0b0d53465a802c48282f227
Glauber Ramos <glauberamos@gmail.com>
2018-11-27 15:43:20 -0200

    Add tests for aboutTypes

--- src/questionnaires/list/__tests__/renderOption.test.js
+++ src/questionnaires/list/__tests__/renderOption.test.js
@@ -4,13 +4,39 @@ import renderOption from '../renderOption'
 
 jest.mock('../createWithParams', () => () => {})
 
-it('render correctly', () => {
+it('render correctly with aboutType Supplier', () => {
   const tree = renderer
     .create(
       renderOption(
         {
           foo: 'bar',
           match: { params: { id: 123 } },
+          aboutType: 'Supplier',
+          currentUser: {
+            suppliers: [
+              {
+                id: 6,
+              },
+            ],
+          },
+        },
+        { id: 123 },
+      )({
+        id: 1234,
+      }),
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('render correctly with aboutType Material', () => {
+  const tree = renderer
+    .create(
+      renderOption(
+        {
+          foo: 'bar',
+          match: { params: { id: 123 } },
+          aboutType: 'Material',
           currentUser: {
             suppliers: [
               {
