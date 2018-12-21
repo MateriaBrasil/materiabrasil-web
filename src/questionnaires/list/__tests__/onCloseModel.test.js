d2fb8cb102f5966af305e8ac61b6cb0c11e1de22
Glauber Ramos <glauberamos@gmail.com>
2018-12-21 09:51:32 -0200

    Update test name

--- src/questionnaires/list/__tests__/onCloseModel.test.js
+++ src/questionnaires/list/__tests__/onCloseModel.test.js
@@ -1,6 +1,6 @@
 import onCloseModel from '../onCloseModel'
 
-it('add state to history', () => {
+it('reloads the material', () => {
   const props = { history: { push: jest.fn() }, match: { params: { id: 12 } } }
   onCloseModel(props)()
   expect(props.history.push).toHaveBeenCalledWith('/materials/12?reload=true')
