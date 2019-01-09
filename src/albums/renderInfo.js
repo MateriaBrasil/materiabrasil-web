adc601b61d93d97d6afcec642870e7e9f8bc8fe0
Glauber Ramos <glauberamos@gmail.com>
2019-01-09 17:25:06 -0200

    Disable turn album private if it is not the logged user

--- src/albums/renderInfo.js
+++ src/albums/renderInfo.js
@@ -1,16 +1,52 @@
-import React from 'react'
-import { Info } from 'croods'
+import React, { Fragment } from 'react'
+import { Info, Edit } from 'croods'
+import Switch from '@material-ui/core/Switch'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Album from '../users/info/Album'
 
 export default props => routeProps => {
   const { match } = routeProps
+  const { currentUser } = props
 
   return (
-    <Info
-      name="albums"
-      id="albums"
-      path={`/albums/${match.params.id}`}
-      render={album => <Album {...album} />}
-    />
+    <Fragment>
+      <Info
+        name="albums"
+        id="albums"
+        path={`/albums/${match.params.id}`}
+        render={album => {
+          const isOwner = currentUser && currentUser.id === album.userId
+
+          return (
+            <Fragment>
+              <Edit
+                id={match.params.id}
+                name="editAlbum"
+                path={`/albums/${match.params.id}`}
+                render={({ info, update, updating, error }) => (
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        disabled={!isOwner}
+                        checked={info.private}
+                        onChange={() => {
+                          update({
+                            id: match.params.id,
+                            private: !info.private,
+                          })
+                        }}
+                      />
+                    }
+                    label="Tornar album privado"
+                  />
+                )}
+                renderUpdated={() => null}
+              />
+              <Album {...album} />
+            </Fragment>
+          )
+        }}
+      />
+    </Fragment>
   )
 }
