7c2f45fd34a74015654906f33cdd5f912c0f0ff9
Glauber Ramos <glauberamos@gmail.com>
2019-01-10 13:29:50 -0200

    Add link to premium

--- src/albums/renderInfo.js
+++ src/albums/renderInfo.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { Info, Edit } from 'croods'
 import Switch from '@material-ui/core/Switch'
+import Link from 'react-router-dom/Link'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Album from '../users/info/Album'
 
@@ -20,28 +21,38 @@ export default props => routeProps => {
           return (
             <Fragment>
               {isOwner && (
-                <Edit
-                  id={match.params.id}
-                  name="editAlbum"
-                  path={`/albums/${match.params.id}`}
-                  render={({ info, update, updating, error }) => (
-                    <FormControlLabel
-                      control={
-                        <Switch
-                          checked={info.private}
-                          onChange={() => {
-                            update({
-                              id: match.params.id,
-                              private: !info.private,
-                            })
-                          }}
-                        />
-                      }
-                      label="Tornar álbum privado"
-                    />
-                  )}
-                  renderUpdated={() => null}
-                />
+                <Link
+                  to={`/albums/${match.params.id}/premium`}
+                  style={{
+                    textDecoration: 'none',
+                  }}
+                >
+                  <Edit
+                    id={match.params.id}
+                    name="editAlbum"
+                    path={`/albums/${match.params.id}`}
+                    render={({ info, update, updating, error }) => (
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            style={{
+                              pointerEvents: 'none',
+                            }}
+                            checked={info.private}
+                            onChange={() => {
+                              update({
+                                id: match.params.id,
+                                private: !info.private,
+                              })
+                            }}
+                          />
+                        }
+                        label="Tornar álbum privado"
+                      />
+                    )}
+                    renderUpdated={() => null}
+                  />
+                </Link>
               )}
               <Album {...album} />
             </Fragment>
