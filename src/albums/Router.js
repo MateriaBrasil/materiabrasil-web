9b082e00e10ff8923d63880a7eb587bc54563fd1
Glauber Ramos <glauberamos@gmail.com>
2019-01-10 12:58:02 -0200

    Add premium page

--- src/albums/Router.js
+++ src/albums/Router.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Route from 'react-router-dom/Route'
 
 import renderInfo from './renderInfo'
+import renderPremium from './renderPremium'
 
 export default props => (
   <div>
-    <Route exact path="/albums/:id" render={renderInfo(props)} />
+    <Route path="/albums/:id" render={renderInfo(props)} />
+    <Route exact path="/albums/:id/premium" render={renderPremium(props)} />
   </div>
 )
