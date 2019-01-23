be373f4e9f9449354a8d6dd4d83d2071170bb5b0
Glauber Ramos <glauberamos@gmail.com>
2019-01-23 11:47:11 -0200

    Small refactor props

--- src/albums/UserMessages.js
+++ src/albums/UserMessages.js
@@ -3,21 +3,23 @@ import Typography from '@material-ui/core/Typography'
 
 export default class extends Component {
   render() {
+    const { currentUser } = this.props
+
     return (
       <Fragment>
-        {this.props.currentUser &&
-          this.props.currentUser.pendingSubscription && (
+        {currentUser &&
+          currentUser.pendingSubscription && (
             <Typography color="inherit" variant="h5">
               Estamos processando sua assinatura! Volte mais tarde.
             </Typography>
           )}
-        {this.props.currentUser &&
-          this.props.currentUser.subscribed && (
+        {currentUser &&
+          currentUser.subscribed && (
             <Typography color="inherit" variant="h5">
               Parabéns! Você ativou sua assinatura com sucesso.
             </Typography>
           )}
-        {!this.props.currentUser && (
+        {!currentUser && (
           <Typography color="inherit">
             Você não está logado, se logue para continuar
           </Typography>
