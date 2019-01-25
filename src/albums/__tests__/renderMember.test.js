0f7d730392b5c3d9816c32dab8fd859405707b38
Glauber Ramos <glauberamos@gmail.com>
2019-01-25 17:04:40 -0200

    Add test for renderMember

---
+++ src/albums/__tests__/renderMember.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import renderMember from '../renderMember'
+
+jest.mock('croods', () => ({
+  New: props => (
+    <div {...props}>
+      New - {props.render(() => {})} - {props.renderCreated()}
+    </div>
+  ),
+}))
+
+jest.mock('materials/Dialog', () => props => (
+  <div {...props}>Dialog - {props.children}</div>
+))
+
+jest.mock('../../payments/form/render', () =>
+  jest.fn(props => routeProps => <div {...props}>render</div>),
+)
+
+jest.mock('react-router-dom/Redirect', () => props => (
+  <div {...props}>Redirect</div>
+))
+
+const routeProps = { match: { params: { id: 123 } } }
+const props = {
+  currentUser: { albums: [{ id: 1, name: '123' }, { id: 2, name: '345' }] },
+}
+
+describe('render', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(renderMember(props)(routeProps)).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
