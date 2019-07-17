0c4fe446ade4f4130c90526eceb9719196a5542d
Peterson Passos <peterson.jfp@gmail.com>
2019-07-17 10:13:45 -0300

    fix bug on sintaxe

--- src/suppliers/info/Materials.js
+++ src/suppliers/info/Materials.js
@@ -37,11 +37,12 @@ export default props => {
       )}
       {materials && (
         <Grid container spacing={8} cellheight={360}>
-          {materials.map((material, index) => {
-            if (isOwner || material.published) {
-              return <Card key={index} ignoreHighlights material={material} />
-            }
-          })}
+          {materials.map(
+            (material, index) =>
+              (isOwner || material.published) && (
+                <Card key={index} ignoreHighlights material={material} />
+              ),
+          )}
         </Grid>
       )}
     </Grid>
