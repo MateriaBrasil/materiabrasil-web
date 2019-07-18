1a3d59f928885e1d072352aea152dbb046a9a011
Peterson Passos <peterson.jfp@gmail.com>
2019-07-18 10:59:28 -0300

    upgrade test

--- src/questionnaires/list/__tests__/renderQuestion.test.js
+++ src/questionnaires/list/__tests__/renderQuestion.test.js
@@ -5,9 +5,46 @@ import renderQuestion from '../renderQuestion'
 
 jest.mock('../renderOption', () => props => <div {...props}>Question</div>)
 
-it('render correctly', () => {
-  const tree = renderer
-    .create(renderQuestion({ foo: 'bar' })({ questionId: 1234 }))
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+const createMockMediaMatcher = matches => () => ({
+  matches,
+  addListener: () => {},
+  removeListener: () => {},
+})
+
+let originalMatchMedia
+beforeAll(() => {
+  originalMatchMedia = window.matchMedia
+})
+afterAll(() => {
+  window.matchMedia = originalMatchMedia
+})
+
+describe('when it page are displayed on a desktop screen', () => {
+  it('renders correctly', () => {
+    window.matchMedia = createMockMediaMatcher(true)
+    const question = {
+      options: {},
+      description: 'FooBar foo bar',
+      helperText: 'helper foo bar',
+    }
+    const tree = renderer
+      .create(renderQuestion({ foo: 'bar' })(question))
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('when it page are displayed on a mobile screen', () => {
+  it('renders correctly', () => {
+    window.matchMedia = createMockMediaMatcher(false)
+    const question = {
+      options: {},
+      description: 'FooBar foo bar',
+      helperText: 'helper foo bar',
+    }
+    const tree = renderer
+      .create(renderQuestion({ foo: 'bar' })(question))
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
