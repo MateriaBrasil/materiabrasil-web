5b5366f7a2ce59b7d395af5b8c638654a4e74365
Peterson Passos <peterson.jfp@gmail.com>
2019-07-18 11:06:15 -0300

    remove env api url from app test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,16 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import App from '../App'
 
+const OLD_ENV = process.env
+beforeEach(() => {
+  jest.resetModules()
+  process.env = { ...OLD_ENV }
+  delete process.env.NODE_ENV
+})
+afterEach(() => {
+  process.env = OLD_ENV
+})
+
 jest.mock('../Router', () => props => <div {...props}>Router</div>)
 jest.mock('croods', () => ({
   createReducer: () => (state = {}, action = {}) => state,
@@ -16,6 +26,7 @@ jest.mock('auth/Auth', () => ({ render, ...props }) => render(props))
 jest.mock('../Screen', () => props => <div {...props}>Screen</div>)
 
 it('renders correctly', () => {
+  process.env.REACT_APP_API_URL = 'https://foo-api.com'
   const tree = renderer.create(<App />).toJSON()
   expect(tree).toMatchSnapshot()
 })
