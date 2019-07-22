cf95bd05ef7f81873738107b9c317a81a3e508e7
Peterson Passos <peterson.jfp@gmail.com>
2019-07-22 11:56:40 -0300

    upgrade test

--- src/materials/info/__tests__/Drivers.test.js
+++ src/materials/info/__tests__/Drivers.test.js
@@ -26,10 +26,10 @@ jest.mock('recharts', () => ({
 describe('when the material has all indicator', () => {
   it('renders correctly', () => {
     const props = {
-      materialityDriver: 50,
-      manufactureDriver: 50,
-      managementDriver: 50,
-      socialDriver: 50,
+      materialityDriver: 32,
+      manufactureDriver: 42,
+      managementDriver: 57,
+      socialDriver: 93,
     }
     const tree = renderer.create(<Drivers {...props} />).toJSON()
     expect(tree).toMatchSnapshot()
@@ -39,7 +39,7 @@ describe('when the material has all indicator', () => {
 describe('when the material has only 1 indicator at least', () => {
   it('renders correctly', () => {
     const props = {
-      materialityDriver: 50,
+      materialityDriver: 45,
       manufactureDriver: -3,
       managementDriver: null,
       socialDriver: null,
