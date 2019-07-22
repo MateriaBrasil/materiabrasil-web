308c0a2510acde5bfe34543289a946a1435606c3
Peterson Passos <peterson.jfp@gmail.com>
2019-07-22 14:24:33 -0300

    upgrade test

--- src/auth/forgotPassword/__tests__/ForgotPassword.test.js
+++ src/auth/forgotPassword/__tests__/ForgotPassword.test.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import ForgotPassword from '../ForgotPassword'
+import ForgotPassword, { passCorrectParams } from '../ForgotPassword'
 
 jest.mock('croods', () => ({ New: props => <div {...props}>New</div> }))
 
 it('renders correctly', () => {
   const tree = renderer.create(<ForgotPassword />).toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+describe('passCorrectParams', () => {
+  it('works correctly', () => {
+    const mock = jest.fn()
+    passCorrectParams(mock)({
+      create: Function,
+      creating: Boolean,
+      error: String,
+    })
+    expect(mock).toHaveBeenCalledWith({
+      create: Function,
+      creating: Boolean,
+      apiError: String,
+    })
+  })
 })
