49fc5483cc6ce3ca2213c8b8390e8fec82473a48
Peterson Passos <peterson.jfp@gmail.com>
2019-07-29 12:09:17 -0300

    Update src/users/info/recalculateTopsis/CreatedMessage.js
    Co-Authored-By: Felipe Freitag Vargas <ffvargas@gmail.com>

--- src/users/info/recalculateTopsis/CreatedMessage.js
+++ src/users/info/recalculateTopsis/CreatedMessage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class extends React.Component {
   componentDidMount() {
     this.props.closeDialog()
-    this.props.snackbar.actions.setMessage('Recalculo disparado com sucesso.')
+    this.props.snackbar.actions.setMessage('Recálculo disparado com sucesso.')
   }
 
   render() {
