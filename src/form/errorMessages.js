bc43bac36ad595f03df30c30fbbbfbec5ad63f02
Willa <willa@iMac-de-Convidado.local>
2019-10-16 11:23:31 -0300

    eslint config

--- src/form/errorMessages.js
+++ src/form/errorMessages.js
@@ -1,4 +1,4 @@
-import Validators from 'redux-form-validators'
+import Validators from 'redux-form-validators';
 
 Object.assign(Validators.messages, {
   email: 'Não é um email válido',
@@ -8,4 +8,4 @@ Object.assign(Validators.messages, {
     'Mínimo de {count, number} {count, plural, one {caracter} other {caracteres}}',
   greaterThanOrEqualTo: 'Deve ser maior ou igual a {count, number}',
   lessThanOrEqualTo: 'Deve ser menor ou igual a {count, number}',
-})
+});
