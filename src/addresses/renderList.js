8ea9ea162432c428de4d6cdecfbeffa336d29fc9
Willa <willa@iMac-de-Convidado.local>
2019-10-18 14:58:31 -0300

    corrigido telefone no endereço

--- src/addresses/renderList.js
+++ src/addresses/renderList.js
@@ -1,18 +1,18 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 
-import map from 'lodash/map'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import Avatar from '@material-ui/core/Avatar'
-import BuildIcon from '@material-ui/icons/Build'
-import PhoneIcon from '@material-ui/icons/Phone'
-import EmailIcon from '@material-ui/icons/Email'
+import map from 'lodash/map';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import BuildIcon from '@material-ui/icons/Build';
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailIcon from '@material-ui/icons/Email';
 
-import Card from './Card'
+import Card from './Card';
 
 export default props => list => {
-  const { phone } = props.current
+  const { phone } = props.current;
   return (
     <Fragment>
       {(list && list.length) > 0 ? (
@@ -25,40 +25,54 @@ export default props => list => {
                 addressType,
                 streetAddress,
                 zipCode,
+                phoneNumber,
                 city,
                 state,
-                country
+                country,
               }) => {
                 const fullAddress = `${streetAddress}${
                   zipCode ? ` - ${zipCode}` : ''
-                }`
-                const fullCity = `${city}, ${state}, ${country}`
+                }`;
+                const fullCity = `${city}, ${state}, ${country}`;
 
                 return (
-                  <ListItem key={id}>
-                    <Avatar>
-                      {addressType === 'manufacture' ? (
-                        <BuildIcon />
-                      ) : (
-                        <EmailIcon />
-                      )}
-                    </Avatar>
-                    <ListItemText primary={fullAddress} secondary={fullCity} />
-                  </ListItem>
-                )
+                  <div>
+                    <ListItem key={id}>
+                      <Avatar>
+                        {addressType === 'manufacture' ? (
+                          <BuildIcon />
+                        ) : (
+                          <EmailIcon />
+                        )}
+                      </Avatar>
+                      <ListItemText
+                        primary={fullAddress}
+                        secondary={fullCity}
+                      />
+                    </ListItem>
+
+                    {phoneNumber ? (
+                      <ListItem>
+                        <Avatar>
+                          <PhoneIcon />
+                        </Avatar>
+                        <ListItemText primary={phoneNumber} />
+                      </ListItem>
+                    ) : phone ? (
+                      <ListItem>
+                        <Avatar>
+                          <PhoneIcon />
+                        </Avatar>
+                        <ListItemText primary={phone} />
+                      </ListItem>
+                    ) : null}
+                  </div>
+                );
               },
-            )}
-            { phone && (
-              <ListItem>
-                <Avatar>
-                  <PhoneIcon />
-                </Avatar>
-                <ListItemText primary={phone} />
-              </ListItem>
             )}
           </List>
         </Card>
       ) : null}
     </Fragment>
-  )
-}
+  );
+};
