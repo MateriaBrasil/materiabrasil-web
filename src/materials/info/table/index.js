486d63e70c14bc14522b147c9064a2a85da87642
Willa <willa@iMac-de-Convidado.local>
2020-01-13 11:15:14 -0300

    correção na tabela de caracteristicas

--- src/materials/info/table/index.js
+++ src/materials/info/table/index.js
@@ -1,61 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
-import _ from 'lodash';
-
 import { StyledTable, StyledRow } from './styles';
-
-import { api } from '../../../services/axios';
 
 export default function(props) {
   const { current } = props;
-  const { categories } = current;
 
-  useEffect(() => {
-    getAllCategories();
-  }, []);
+  const visuais = current.categories.filter(x => x.parentId === 243);
+  const tateis = current.categories.filter(x => x.parentId === 250);
+  const resistente = current.categories.filter(x => x.parentId === 257);
+  const forma = current.categories.filter(x => x.parentId === 265);
 
-  const [visuais, setVisuais] = useState([]);
-  const [tateis, setTateis] = useState([]);
-  const [resistente, setResistente] = useState([]);
-  const [forma, setForma] = useState([]);
-
-  async function getAllCategories() {
-    const data = await api.get('/categories/caracteristicas');
-    const result = data.data;
-
-    const compare_visuais = result['children'][0];
-    const compare_tateis = result['children'][1];
-    const compare_resistente = result['children'][2];
-    const compare_forma = result['children'][3];
-
-    const _visuais = _(compare_visuais.children)
-      .differenceBy(categories, 'name')
-      .map(_.partial(_.pick, _, 'name'))
-      .value();
-
-    setVisuais(_visuais.map(v => v.name));
-
-    const _tateis = _(compare_tateis.children)
-      .differenceBy(categories, 'name')
-      .map(_.partial(_.pick, _, 'name'))
-      .value();
-
-    setTateis(_tateis.map(t => t.name));
-
-    const _resistente = _(compare_resistente.children)
-      .differenceBy(categories, 'name')
-      .map(_.partial(_.pick, _, 'name'))
-      .value();
-
-    setResistente(_resistente.map(r => r.name));
-
-    const _forma = _(compare_forma.children)
-      .differenceBy(categories, 'name')
-      .map(_.partial(_.pick, _, 'name'))
-      .value();
-
-    setForma(_forma.map(f => f.name));
-  }
+  console.log(visuais);
+  console.log(tateis);
+  console.log(resistente);
+  console.log(forma);
 
   return (
     <StyledTable>
@@ -64,61 +22,63 @@ export default function(props) {
         <span>Têxtil</span>
       </StyledRow> */}
 
-      {/* <StyledRow expanded={false}>
+      <StyledRow expanded={false}>
         <span>Forma</span>
         <span className="row-content">
-          {forma &&
-            forma.map(
-              (f, index) => (index === forma.length - 1 ? `${f}` : `${f}, `),
-            )}
+          {forma.length > 0
+            ? forma.map(
+                (f, index) =>
+                  index === forma.length - 1 ? `${f.name}` : `${f.name}, `,
+              )
+            : `N/A`}
         </span>
       </StyledRow>
-
       <StyledRow expanded={false}>
         <span>Visuais</span>
         <span className="row-content">
-          {visuais &&
-            visuais.map(
-              (v, index) => (index === visuais.length - 1 ? `${v}` : `${v}, `),
-            )}
+          {visuais.length > 0
+            ? visuais.map(
+                (v, index) =>
+                  index === visuais.length - 1 ? `${v.name}` : `${v.name}, `,
+              )
+            : `N/A`}
         </span>
       </StyledRow>
-
       <StyledRow expanded={false}>
         <span>Tátei</span>
         <span className="row-content">
-          {tateis &&
-            tateis.map(
-              (t, index) => (index === tateis.length - 1 ? `${t}` : `${t}, `),
-            )}
+          {tateis.length > 0
+            ? tateis.map(
+                (t, index) =>
+                  index === tateis.length - 1 ? `${t.name}` : `${t.name}, `,
+              )
+            : `N/A`}
         </span>
       </StyledRow>
-
       <StyledRow expanded={false}>
         <span>Resistente a</span>
         <span className="row-content">
-          {resistente &&
-            resistente.map(
-              (r, index) =>
-                index === resistente.length - 1 ? `${r}` : `${r}, `,
-            )}
+          {resistente.length > 0
+            ? resistente.map(
+                (r, index) =>
+                  index === resistente.length - 1 ? `${r.name}` : `${r.name}, `,
+              )
+            : `N/A`}
         </span>
-      </StyledRow> */}
+      </StyledRow>
 
       {/* <StyledRow>
         <span>Processabilidade</span>
         <span>Têxtil</span>
       </StyledRow> */}
-
       <StyledRow>
         <span>Densidade/gramatura</span>
         <span>{current.density ? current.density : `N/A`}</span>
       </StyledRow>
-
-      <StyledRow>
+      {/* <StyledRow>
         <span>Anexo 1</span>
         <span>N/A</span>
-      </StyledRow>
+      </StyledRow> */}
     </StyledTable>
   );
 }
