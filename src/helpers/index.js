98a8176a30bcd467234c424603edf4ae59fd5530
Willa <willa@imacdeconvidado.home>
2020-01-27 16:39:21 -0300

    correção dos links invalidos na pagina fornecedor

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,21 @@
 export function addhttp(url) {
-  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+  if (!/^(?:f|ht)tp?\:\/\//.test(url) || !/^(?:f|ht)tps?\:\/\//.test(url)) {
     url = 'http://' + url;
   }
   return url;
+}
+
+export function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i',
+  ); // fragment locator
+  return !!pattern.test(str);
 }
 
 export function phoneMask(v) {
