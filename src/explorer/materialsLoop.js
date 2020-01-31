fb33f92c9774382308dd1d029496e215c5001e82
Willa <willa@imacdeconvidado.home>
2020-01-31 14:41:34 -0300

    index com materiais aleatorios

--- src/explorer/materialsLoop.js
+++ src/explorer/materialsLoop.js
@@ -18,50 +18,130 @@ export default function(props) {
   const { currentUser, categories } = props;
   const { term } = props;
 
+  const [firstLoad, setFirstLoad] = useState(true);
   const [materials, setMaterials] = useState([]);
   const [materialsPassed, setMaterialsPassed] = useState(null);
-  const [loadingMore, setLoadingMore] = useState(true);
-  const [loadingCat, setLoadingCat] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [loadingCat, setLoadingCat] = useState(false);
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
-
   const nextPage = () => setPage(p => p + 1);
 
-  function filterCategories() {
-    const endcoded_term = encodeURI(props.term);
-    if (endcoded_term === '') {
-      return `/materials?${categories}`;
-    } else {
-      return `/search?term=${endcoded_term}&${categories}`;
-    }
-  }
-
+  // pagination
   useEffect(
     () => {
       async function loadMaterials() {
         try {
-          console.log('triggeg cat term');
-          console.log(filterCategories());
           setLoadingMore(true);
-          setPage(1);
-          const response = await api.get(filterCategories(), {
+          if (term !== '') {
+            setMaterialsPassed(null);
+          }
+          const path =
+            term === ''
+              ? `/materials?${categories}`
+              : `/search?term=${encodeURI(props.term)}&${categories}`;
+          const response = await api.get(path, {
             params: {
               materials_passed: encodeURI(materialsPassed),
             },
           });
 
-          setMaterials(response.data);
-          setLoadingMore(false);
+          if (response.data === []) {
+            setError('Sem mais resultados');
+          }
+
+          const newMaterials = [...materials, ...response.data];
+          setMaterials(newMaterials);
           setLoadingCat(false);
+          setLoadingMore(false);
+        } catch (err) {
+          // toast.error('Nenhum aluno foi encontrado');
+          setError('Sem mais resultados');
+          setLoadingCat(false);
+          setLoadingMore(false);
+        }
+      }
+      console.log(categories);
+      console.log(term);
+      loadMaterials();
+      console.log('pagination effect');
+    },
+    [page],
+  );
+
+  // categories
+  useEffect(
+    () => {
+      setMaterialsPassed(null);
+
+      async function loadMaterials() {
+        try {
+          setLoadingCat(true);
+          const response = await api.get(`/materials?${categories}`, {
+            params: {
+              materials_passed: encodeURI(materialsPassed),
+            },
+          });
+
+          if (response.data === []) {
+            setError('Sem mais resultados');
+          }
+
+          setMaterials(response.data);
+          setLoadingCat(false);
+          setLoadingMore(false);
+        } catch (err) {
+          // toast.error('Nenhum aluno foi encontrado');
+          setError('Sem mais resultados');
+          setLoadingCat(false);
+          setLoadingMore(false);
+        }
+      }
+      console.log(`>>>> ${categories}`);
+      if (categories !== '' && firstLoad) {
+        return;
+      } else {
+        loadMaterials();
+        setFirstLoad(false);
+      }
+    },
+    [categories],
+  );
+
+  // search
+  useEffect(
+    () => {
+      setMaterialsPassed(null);
+
+      async function loadMaterials() {
+        try {
+          setLoadingCat(true);
+          const endcoded_term = encodeURI(props.term);
+          const response = await api.get(
+            `/search?term=${endcoded_term}&${categories}`,
+            {
+              params: {
+                materials_passed: encodeURI(materialsPassed),
+              },
+            },
+          );
+
+          setMaterials(response.data);
+          setLoadingCat(false);
+          setLoadingMore(false);
         } catch (err) {
           // toast.error('Nenhum material foi encontrado');
+          setError('Nenhum Material encontrado');
+          setLoadingCat(false);
+          setLoadingMore(false);
         }
       }
 
-      setMaterialsPassed(null);
-      setTimeout(loadMaterials, 100);
+      if (term !== '') {
+        loadMaterials();
+      }
     },
-    [categories, term],
+    [term],
   );
 
   useEffect(
@@ -77,34 +157,8 @@ export default function(props) {
     [materials],
   );
 
-  useEffect(
-    () => {
-      async function loadMaterials() {
-        try {
-          console.log('trigged page');
-          setLoadingMore(true);
-          const response = await api.get(filterCategories(), {
-            params: {
-              materials_passed: encodeURI(materialsPassed),
-            },
-          });
-
-          const newMaterials = [...materials, ...response.data];
-          setMaterials(newMaterials);
-          setLoadingMore(false);
-          setLoadingCat(false);
-        } catch (err) {
-          // toast.error('Nenhum aluno foi encontrado');
-        }
-      }
-
-      setTimeout(loadMaterials, 100);
-    },
-    [page],
-  );
-
   if (loadingCat) return <Loading />;
-  if (error) return <div>{error}</div>;
+  if (error) return <h1 style={{ marginTop: '30px' }}>{error}</h1>;
 
   return (
     <div>
